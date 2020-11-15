903f3446f1c0a763ec4c96a667f80e33f52c8821
Aleh Karasik <karasik.oleg.nikolaevich@gmail.com>
2020-11-15 14:16:59 +0300

    attempt to use shared class in code.

--- src/external/generalized-blocked-fw-algorithm/single-threaded.cpp
+++ src/external/generalized-blocked-fw-algorithm/single-threaded.cpp
@@ -2,11 +2,14 @@
 #include <vector>
 #include <string>
 
+#include "../../utilz/matrix.h"
+
 using namespace std;
 
 int main()
 {
     vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    math::utilz::square_matrix<long> a;
 
     for (const string& word : msg)
     {
