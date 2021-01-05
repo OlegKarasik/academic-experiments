3393ca603960ace425203e5fa0963b5499321a26
Aleh Karasik <karasik.oleg.nikolaevich@gmail.com>
2021-01-05 18:22:44 +0300

    replaced formatting rules. replaced rectangular shape with square.

--- src/external/fw-algorithm/single-threaded.cpp
+++ src/external/fw-algorithm/single-threaded.cpp
@@ -5,50 +5,41 @@
 
 using namespace std;
 
-template <typename T>
-class rect_shape {
+template <typename T> class square_shape {
   using size_type = size_t;
-  using pointer = T*;
-  using reference = T&;
+  using pointer = T *;
+  using reference = T &;
   using value_type = T;
 
-  private:
-  const size_type m_w;
-  const size_type m_h;
+private:
+  const size_type m_s;
 
   pointer m_p;
 
-  public:
+public:
   class iterator {
-private:
+  private:
     pointer m_p;
 
-public:
-    iterator(pointer data)
-        : m_p(data)
-    {
-    }
+  public:
+    iterator(pointer data) : m_p(data) {}
 
-    iterator& operator++()
-    {
+    iterator &operator++() {
       ++this->m_p;
       return *this;
     }
-    iterator& operator++(int)
-    {
+    iterator &operator++(int) {
       iterator p = this->m_p;
 
       ++(*this);
 
       return *this;
     }
-    iterator& operator--()
-    {
+    iterator &operator--() {
       --this->m_p;
       return *this;
     }
-    iterator& operator--(int)
-    {
+    iterator &operator--(int) {
       iterator p = this->m_p;
 
       --(*this);
@@ -56,98 +47,57 @@ public:
       return *this;
     }
 
-    reference operator*()
-    {
-      return *this->m_p;
-    }
-    reference operator->()
-    {
-      return *this->m_p;
-    }
+    reference operator*() { return *this->m_p; }
+    reference operator->() { return *this->m_p; }
 
-    iterator& operator=(pointer v)
-    {
+    iterator &operator=(pointer v) {
       this->m_p = v;
       return *this;
     }
-    iterator& operator=(const iterator& o)
-    {
+    iterator &operator=(const iterator &o) {
       this->m_p = o.m_p;
       return *this;
     }
 
-    reference operator[](size_type idx)
-    {
-      return this->m_p[idx];
-    }
-    const reference operator[](size_type idx) const
-    {
-      return this->m_p[idx];
-    }
+    reference operator[](size_type idx) { return this->m_p[idx]; }
+    const reference operator[](size_type idx) const { return this->m_p[idx]; }
 
-    bool operator==(const iterator& o) const noexcept
-    {
+    bool operator==(const iterator &o) const noexcept {
       return this == &o || this->m_p == o.m_p;
     };
-    bool operator!=(const iterator& o) const noexcept
-    {
-      return !(*this == o);
-    };
+    bool operator!=(const iterator &o) const noexcept { return !(*this == o); };
   };
 
-  rect_shape(pointer data, size_type width, size_type height)
-      : m_p(data)
-      , m_w(width)
-      , m_h(height)
-  {
-  }
+  square_shape(pointer data, size_type size)
+      : m_p(data), m_s(size) {}
 
-  size_type w() { return this->m_w; }
-  size_type h() { return this->m_h; }
+  size_type s() { return this->m_s; }
 
-  iterator begin()
-  {
-    return iterator(this->m_p);
-  }
-  iterator end()
-  {
-    return iterator(this->m_p + (this->m_w * this->m_h));
-  }
+  iterator begin() { return iterator(this->m_p); }
+  iterator end() { return iterator(this->m_p + (this->m_s * this->m_s)); }
 
-  pointer operator[](size_type idx)
-  {
-    return &this->m_p[idx * this->m_w];
-  }
-  pair<iterator, iterator>& operator()(size_type idx)
-  {
-    return make_pair<iterator, iterator>(
-        iterator(this->m_p),
-        iterator(this->m_p + this->m_w));
-  }
+  pointer operator[](size_type idx) { return &this->m_p[idx * this->m_s]; }
 
-  bool operator==(const rect_shape& o) const noexcept
-  {
-    return this == &o || (this->m_p == o.m_p && this->m_w == o.m_w && this->m_h == o.m_h);
+  bool operator==(const square_shape &o) const noexcept {
+    return this == &o || (this->m_p == o.m_p && this->m_s == o.m_s && this->m_h == o.m_h);
   };
-  bool operator!=(const rect_shape& o) const noexcept
-  {
-    return !(*this == o);
-  };
+  bool operator!=(const square_shape &o) const noexcept { return !(*this == o); };
 };
 
-constexpr long no_edge_value() { return ((std::numeric_limits<long>::max)() / 2L) - 1L; };
+constexpr long no_edge_value() {
+  return ((std::numeric_limits<long>::max)() / 2L) - 1L;
+};
 
-void _impl(rect_shape<long>& shape)
-{
-  for (size_t k = 0; k < shape.w(); ++k) {
-    long* kr = shape[k];
+void _impl(square_shape<long> &shape) {
+  for (size_t k = 0; k < shape.s(); ++k) {
+    long *kr = shape[k];
 
-    for (size_t i = 0; i < shape.w(); ++i) {
-      long* ir = shape[i];
-      long* r = kr;
+    for (size_t i = 0; i < shape.s(); ++i) {
+      long *ir = shape[i];
+      long *r = kr;
 
       long v = ir[k];
-      for (size_t j = 0; j < shape.w(); ++j, ++ir, ++r) {
+      for (size_t j = 0; j < shape.s(); ++j, ++ir, ++r) {
         long distance = v + *r;
         if (*ir > distance)
           *ir = distance;
@@ -156,8 +106,7 @@ void _impl(rect_shape<long>& shape)
   };
 };
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
   // if (argc < 2) {
   //     std::cerr << "Usage: " << argv[0] << ""
   //         << "Options:\n"
@@ -167,14 +116,14 @@ int main(int argc, char* argv[])
   // }
   size_t matrix_sz = 7;
 
-  long* matrix = (long*)malloc(matrix_sz * matrix_sz * sizeof(long));
+  long *matrix = (long *)malloc(matrix_sz * matrix_sz * sizeof(long));
   if (matrix == nullptr) {
     std::cerr << "erro: can't allocate memory to hold input matrix";
     return 1;
   }
 
   // Create rectangular shape
-  rect_shape<long> shape(matrix, matrix_sz, matrix_sz);
+  square_shape<long> shape(matrix, matrix_sz);
 
   // Fill it with default values
   std::fill(shape.begin(), shape.end(), no_edge_value());
@@ -189,8 +138,8 @@ int main(int argc, char* argv[])
 
   _impl(shape);
 
-  for (size_t i = 0; i < shape.h(); ++i)
-    for (size_t j = 0; j < shape.w(); ++j)
+  for (size_t i = 0; i < shape.s(); ++i)
+    for (size_t j = 0; j < shape.s(); ++j)
       if (shape[i][j] != no_edge_value())
         std::cout << "m[" << i << "," << j << "] = " << shape[i][j] << endl;
 
