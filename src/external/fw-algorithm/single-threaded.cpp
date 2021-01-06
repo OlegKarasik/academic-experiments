93c8e545dc771642b043a4ed685c01f0664a4256
Aleh Karasik <karasik.oleg.nikolaevich@gmail.com>
2021-01-06 03:50:30 +0300

    separated square shape into separate file

--- src/external/fw-algorithm/single-threaded.cpp
+++ src/external/fw-algorithm/single-threaded.cpp
@@ -1,93 +1,13 @@
 #include <algorithm>
 #include <iostream>
-#include <string>
-#include <vector>
+
+#include "../../utilz/square_shape.h"
 
 using namespace std;
-
-template <typename T> class square_shape {
-  using size_type = size_t;
-  using pointer = T *;
-  using reference = T &;
-  using value_type = T;
-
-private:
-  const size_type m_s;
-
-  pointer m_p;
-
-public:
-  class iterator {
-  private:
-    pointer m_p;
-
-  public:
-    iterator(pointer data) : m_p(data) {}
-
-    iterator &operator++() {
-      ++this->m_p;
-      return *this;
-    }
-    iterator &operator++(int) {
-      iterator p = this->m_p;
-
-      ++(*this);
-
-      return *this;
-    }
-    iterator &operator--() {
-      --this->m_p;
-      return *this;
-    }
-    iterator &operator--(int) {
-      iterator p = this->m_p;
-
-      --(*this);
-
-      return *this;
-    }
-
-    reference operator*() { return *this->m_p; }
-    reference operator->() { return *this->m_p; }
-
-    iterator &operator=(pointer v) {
-      this->m_p = v;
-      return *this;
-    }
-    iterator &operator=(const iterator &o) {
-      this->m_p = o.m_p;
-      return *this;
-    }
-
-    reference operator[](size_type idx) { return this->m_p[idx]; }
-    const reference operator[](size_type idx) const { return this->m_p[idx]; }
-
-    bool operator==(const iterator &o) const noexcept {
-      return this == &o || this->m_p == o.m_p;
-    };
-    bool operator!=(const iterator &o) const noexcept { return !(*this == o); };
-  };
-
-  square_shape(pointer data, size_type size) : m_p(data), m_s(size) {}
-
-  size_type s() { return this->m_s; }
-
-  iterator begin() { return iterator(this->m_p); }
-  iterator end() { return iterator(this->m_p + (this->m_s * this->m_s)); }
-
-  pointer operator[](size_type idx) { return &this->m_p[idx * this->m_s]; }
-
-  bool operator==(const square_shape &o) const noexcept {
-    return this == &o ||
-           (this->m_p == o.m_p && this->m_s == o.m_s && this->m_h == o.m_h);
-  };
-  bool operator!=(const square_shape &o) const noexcept {
-    return !(*this == o);
-  };
-};
+using namespace utilz;
 
 constexpr long no_edge_value() {
-  return ((std::numeric_limits<long>::max)() / 2L) - 1L;
+  return ((std::numeric_limits<long>::max)() / 2) - 1;
 };
 
 void _impl(square_shape<long> &shape) {
