23d55f257237bb8a3a1b2b3c8bd225db47ae70a0
Aleh Karasik <karasik.oleg.nikolaevich@gmail.com>
2021-01-06 23:20:48 +0300

    implemented basic output for grapsh and simple shapes with () operator.

--- src/utilz/io.h
+++ src/utilz/io.h
@@ -86,22 +86,50 @@ scanf_fmt()
   return _scanf_value_fmt<T>();
 };
 
+template<typename G, typename A>
+class graph_output
+{
+private:
+  A& m_allocation_scope;
+
+  G m_graph;
+
+public:
+  graph_output(A& allocation_scope)
+    : m_allocation_scope(allocation_scope)
+  {}
+
+  void prep(size_t vertex_count, size_t edge_count)
+  {
+    this->m_graph = this->m_allocation_scope.allocate(vertex_count, edge_count);
+  }
+  void write(size_t i, size_t j, long v)
+  {
+    this->m_graph(i, j) = v;
+  }
+
+  G& operator()()
+  {
+    return this->m_graph;
+  }
+};
+
 template<typename T, typename O>
 void
-scan_graph_from_dimacs9_file(const std::string& file_path, O& out)
+scan_graph_from_dimacs9_file(const std::string& path, O& out)
 {
   FILE* p_file;
-  if (::fopen_s(&p_file, file_path.c_str(), "r"))
+  if (::fopen_s(&p_file, path.c_str(), "r"))
     throw std::runtime_error("erro: can't open a file");
 
-  char buffer[iobuf_sz];
-  if (::setvbuf(p_file, buffer, _IOFBF, iobuf_sz))
+  char buffer[BUFSIZ];
+  if (::setvbuf(p_file, buffer, _IOFBF, BUFSIZ))
     throw std::runtime_error("erro: can't initialize a buffer");
 
   try {
     const std::string error_msg = "erro: bad file format";
-    const std::string fmt_s = "%zu %zu " + utilz::scanf_fmt<T>() + '\n';
-    const char* fmt = fmt_s.c_str();
+    const std::string fmt_s     = "%zu %zu " + utilz::scanf_fmt<T>() + '\n';
+    const char*       fmt       = fmt_s.c_str();
 
     size_t vertex_count, edge_count;
     if (::feof(p_file) || ::fscanf_s(p_file, "%zu\n%zu", &vertex_count, &edge_count) != 2 || ::ferror(p_file))
@@ -110,7 +138,7 @@ scan_graph_from_dimacs9_file(const std::string& file_path, O& out)
 
     out.prep(vertex_count, edge_count);
 
-    T v;
+    T      v;
     size_t i, j;
     for (size_t edge = 0; edge < edge_count; ++edge) {
       if (::feof(p_file) || ::fscanf_s(p_file, fmt, &i, &j, &v) != 3 || ::ferror(p_file))
