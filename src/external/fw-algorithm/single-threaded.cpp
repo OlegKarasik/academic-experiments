ef49e2400856dd843555d6825abb40a313176650
Aleh Karasik <karasik.oleg.nikolaevich@gmail.com>
2021-01-06 14:20:13 +0300

    updated code to use new features.

--- src/external/fw-algorithm/single-threaded.cpp
+++ src/external/fw-algorithm/single-threaded.cpp
@@ -1,23 +1,27 @@
 #include <algorithm>
-#include <iostream>
 #include <array>
+#include <iostream>
 #include <tuple>
 
 #include "../../utilz/square_shape.h"
 
 using namespace utilz;
 
-constexpr long no_edge_value() {
+constexpr long
+no_edge_value()
+{
   return ((std::numeric_limits<long>::max)() / 2) - 1;
 };
 
-void _impl(square_shape<long> &shape) {
+void
+_impl(square_shape<long>& shape)
+{
   for (size_t k = 0; k < shape.s(); ++k) {
-    long *k_row = shape[k];
+    long* k_row = shape[k];
 
     for (size_t i = 0; i < shape.s(); ++i) {
-      long *i_row = shape[i];
-      long *k_row_l = k_row;
+      long* i_row = shape[i];
+      long* k_row_l = k_row;
 
       long ik = i_row[k];
       for (size_t j = 0; j < shape.s(); ++j, ++i_row, ++k_row_l) {
@@ -29,7 +33,29 @@ void _impl(square_shape<long> &shape) {
   };
 };
 
-int main(int argc, char *argv[]) {
+struct graph_out
+{
+  square_shape<long> shape;
+
+  void prep(const size_t& vertex_count, const size_t& edge_count)
+  {
+    long* p = (long*)malloc(vertex_count * vertex_count * sizeof(long));
+    if (p == nullptr) {
+      throw std::runtime_error("erro: can't allocate memory to hold input matrix");
+    }
+    shape = square_shape<long>(p, vertex_count);
+
+    std::fill(shape.begin(), shape.end(), no_edge_value());
+  }
+  void write(const size_t i, const size_t j, const long& v)
+  {
+    range_check_set<long>(shape, i, j, v);
+  }
+};
+
+int
+main(int argc, char* argv[])
+{
   // if (argc < 2) {
   //     std::cerr << "Usage: " << argv[0] << ""
   //         << "Options:\n"
@@ -37,19 +63,6 @@ int main(int argc, char *argv[]) {
   //         << "\t-d,--destination DESTINATION\tSpecify the destination path"
   //         << std::endl;
   // }
-  size_t matrix_sz = 7;
-
-  long *matrix = (long *)malloc(matrix_sz * matrix_sz * sizeof(long));
-  if (matrix == nullptr) {
-    std::cerr << "erro: can't allocate memory to hold input matrix";
-    return 1;
-  }
-
-  // Create rectangular shape
-  square_shape<long> shape(matrix, matrix_sz);
-
-  // Fill it with default values
-  std::fill(shape.begin(), shape.end(), no_edge_value());
 
   std::array<std::tuple<long, long, long>, 7> input = {
     make_tuple(1, 2, 9),
@@ -61,7 +74,18 @@ int main(int argc, char *argv[]) {
     make_tuple(4, 2, 1)
   };
 
-  fill_shape(shape, input.begin(), input.end());
+  graph_out out;
+  out.prep(7, 7);
+  std::for_each(input.begin(), input.end(), [&out](auto &input) -> void {
+    long v;
+    size_t i, j;
+
+    std::tie(i, j, v) = input;
+
+    out.write(i, j, v);
+  });
+
+  square_shape<long> shape = out.shape;
 
   _impl(shape);
 
