c7ba3efbc978c72f1f9059f446b21711c1e5752d
Aleh Karasik <karasik.oleg.nikolaevich@gmail.com>
2021-01-06 14:20:00 +0300

    updated formatting, replaced fill shape with range_check_set procedure. added copy constructor and copy + move assignment operators.

--- src/utilz/square_shape.h
+++ src/utilz/square_shape.h
@@ -2,41 +2,49 @@ using namespace std;
 
 namespace utilz {
 
-template <typename T> class square_shape {
-  using size_type = size_t;
-  using pointer = T *;
-  using reference = T &;
+template<typename T>
+class square_shape
+{
+  using pointer = T*;
+  using reference = T&;
   using value_type = T;
 
 private:
-  const size_type m_s;
+  size_t m_s;
 
   pointer m_p;
 
 public:
-  class iterator {
+  class iterator
+  {
   private:
     pointer m_p;
 
   public:
-    iterator(pointer data) : m_p(data) {}
+    iterator(pointer data)
+      : m_p(data)
+    {}
 
-    iterator &operator++() {
+    iterator& operator++()
+    {
       ++this->m_p;
       return *this;
     }
-    iterator &operator++(int) {
+    iterator& operator++(int)
+    {
       iterator p = this->m_p;
 
       ++(*this);
 
       return *this;
     }
-    iterator &operator--() {
+    iterator& operator--()
+    {
       --this->m_p;
       return *this;
     }
-    iterator &operator--(int) {
+    iterator& operator--(int)
+    {
       iterator p = this->m_p;
 
       --(*this);
@@ -47,55 +55,84 @@ public:
     reference operator*() { return *this->m_p; }
     reference operator->() { return *this->m_p; }
 
-    iterator &operator=(pointer v) {
+    iterator& operator=(pointer v)
+    {
       this->m_p = v;
       return *this;
     }
-    iterator &operator=(const iterator &o) {
+    iterator& operator=(const iterator& o)
+    {
       this->m_p = o.m_p;
       return *this;
     }
 
-    reference operator[](size_type idx) { return this->m_p[idx]; }
-    const reference operator[](size_type idx) const { return this->m_p[idx]; }
+    reference operator[](size_t idx) { return this->m_p[idx]; }
+    const reference operator[](size_t idx) const { return this->m_p[idx]; }
 
-    bool operator==(const iterator &o) const noexcept {
+    bool operator==(const iterator& o) const noexcept
+    {
       return this == &o || this->m_p == o.m_p;
     };
-    bool operator!=(const iterator &o) const noexcept { return !(*this == o); };
+    bool operator!=(const iterator& o) const noexcept { return !(*this == o); };
   };
 
-  square_shape(pointer data, size_type size) : m_p(data), m_s(size) {}
+  square_shape()
+    : m_p(nullptr)
+    , m_s(0)
+  {}
+  square_shape(const square_shape &o)
+    : m_p(o.m_p)
+    , m_s(o.m_s)
+  {}
+  square_shape(pointer data, size_t size)
+    : m_p(data)
+    , m_s(size)
+  {}
 
-  size_type s() { return this->m_s; }
+  pointer p() { return this->m_p; }
+  size_t s() { return this->m_s; }
 
   iterator begin() { return iterator(this->m_p); }
   iterator end() { return iterator(this->m_p + (this->m_s * this->m_s)); }
 
-  pointer operator[](size_type idx) { return &this->m_p[idx * this->m_s]; }
+  pointer operator[](size_t idx) { return &this->m_p[idx * this->m_s]; }
 
-  bool operator==(const square_shape &o) const noexcept {
-    return this == &o ||
-           (this->m_p == o.m_p && this->m_s == o.m_s && this->m_h == o.m_h);
+  bool operator==(const square_shape& o) const noexcept
+  {
+    return this == &o || (this->m_p == o.m_p && this->m_s == o.m_s && this->m_h == o.m_h);
   };
-  bool operator!=(const square_shape &o) const noexcept {
+  bool operator!=(const square_shape& o) const noexcept
+  {
     return !(*this == o);
   };
+
+  square_shape& operator=(const square_shape& o)
+  {
+    if (this != &o) {
+      this->m_s = o.m_s;
+      this->m_p = o.m_p;
+    }
+    return *this;
+  }
+
+  square_shape& operator=(square_shape&& o) noexcept
+  {
+    if (this != &o) {
+      this->m_s = std::exchange(o.m_s, 0);
+      this->m_p = std::exchange(o.m_p, nullptr);
+    }
+    return *this;
+  }
 };
 
-template<typename T, typename Iterator>
-void fill_shape(square_shape<T> &shape, Iterator &first, Iterator &limit) {
-  for (; first != limit; ++first) {
-    std::tuple<T, T, T> t = *first;
+template<typename T>
+void
+range_check_set(square_shape<T>& shape, size_t i, size_t j, const T& v)
+{
+  if (shape.s() <= i || shape.s() <= j)
+    throw std::out_of_range("erro: shape isn\'t large enough to hold the data");
 
-    T i, j, v;
-    std::tie(i, j, v) = t;
-
-    if (shape.s() <= i || shape.s() <= j)
-      throw std::out_of_range("erro: shape isn\'t large enough to hold the data");
-
-    shape[i][j] = v;
-  }
+  shape[i][j] = v;
 }
 
 } // namespace utilz
