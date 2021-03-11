1a21dd34d334ae0c4bd6575d31e605cd5e44b8df
Aleh Karasik <karasik.oleg.nikolaevich@gmail.com>
2021-03-12 00:06:11 +0300

    Removed extra burden from application.cpp

--- src/apsp/fw/00/_application.cpp
+++ src/apsp/fw/00/_application.cpp
@@ -1,8 +1,4 @@
-#include <algorithm>
-#include <chrono>
 #include <iostream>
-#include <random>
-#include <string>
 
 // global utilz
 #include "measure.hpp"
@@ -24,26 +20,14 @@ using matrix_get_size = gutilz::procedures::square_shape_get_size<matrix>;
 int
 main(int argc, char* argv[]) noexcept
 {
-  if (!std::cin) {
-    std::cerr << "erro: no input; expected a stream which represents "
-                 "a matrix in the following format: "
-                 "<size> / <from> <to> <count> ... <from> <to> <count>";
-    return 1;
-  }
-
   matrix m;
 
-  std::cerr
-    << "Scan: "
-    << gutilz::measure_milliseconds([&m]() -> void { lutilz::io::scan_matrix(std::cin, m); })
-    << "ms" << std::endl;
+  auto scan_ms = gutilz::measure_milliseconds([&m]() -> void { lutilz::io::scan_matrix(std::cin, m); });
+  std::cerr << "Scan: " << scan_ms << "ms" << std::endl;
 
-  std::cerr
-    << "Exec: "
-    << gutilz::measure_milliseconds([&m]() -> void { calculate_apsp(m); })
-    << "ms" << std::endl;
+  auto exec_ms = gutilz::measure_milliseconds([&m]() -> void { calculate_apsp(m); });
+  std::cerr << "Exec: " << exec_ms << "ms" << std::endl;
 
-  std::cerr << "Prnt"
-    << gutilz::measure_milliseconds([&m]() -> void { lutilz::io::print_matrix(std::cout, m); })
-    << "ms" << std::endl;
+  auto prnt_ms = gutilz::measure_milliseconds([&m]() -> void { lutilz::io::print_matrix(std::cout, m); });
+  std::cerr << "Prnt: " << prnt_ms << "ms" << std::endl;
 }
