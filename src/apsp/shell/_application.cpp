36e90fccdad931b9d9bd130da71563c1a4a6ae5d
Aleh Karasik <karasik.oleg.nikolaevich@gmail.com>
2021-10-31 19:20:36 +0300

    implemented input / output from a file

--- src/apsp/shell/_application.cpp
+++ src/apsp/shell/_application.cpp
@@ -10,8 +10,13 @@
 
 // global includes
 //
+#include <fstream>
 #include <iostream>
 #include <memory>
+
+// global C includes
+//
+#include <unistd.h>
 
 // global utilz
 //
@@ -36,30 +41,52 @@ using matrix = ::utilz::square_shape<g_calculation_type, g_allocator_type>;
 int
 main(int argc, char* argv[]) noexcept
 {
+  std::ifstream ins;
+  std::ofstream outs;
+
 #ifdef APSP_ALG_BLOCKED
   matrix::size_type s;
-  if (argc > 1) {
-    s = atoi(argv[1]);
-  } else {
-    std::cout << "Please specify a block size" << std::endl;
-    return 1;
-  }
+
+  const char* options = "i:o:b:";
+#else
+  const char* options = "i:o:";
 #endif
+
+  int opt;
+  while ((opt = getopt(argc, argv, options)) != -1) {
+    switch (opt) {
+      case 'i':
+        ins = std::ifstream(optarg);
+        break;
+      case 'o':
+        outs = std::ofstream(optarg);
+        break;
+#ifdef APSP_ALG_BLOCKED
+      case 'b':
+        s = atoi(optarg);
+        break;
+#endif
+    }
+  }
+
+  std::istream& in  = ins.is_open() ? ins : std::cin;
+  std::ostream& out = outs.is_open() ? outs : std::cout;
 
   // Define matrix and execute algorithm specific overloads of methods
   //
   matrix m;
 
 #ifdef APSP_ALG_BLOCKED
-  auto scan_ms = utilz::measure_milliseconds([&m, s]() -> void { ::apsp::io::scan_matrix(std::cin, m, s); });
+  auto scan_ms = utilz::measure_milliseconds([&m, &in, s]() -> void { ::apsp::io::scan_matrix(in, m, s); });
 #else
-  auto scan_ms = utilz::measure_milliseconds([&m]() -> void { ::apsp::io::scan_matrix(std::cin, m); });
+  auto scan_ms = utilz::measure_milliseconds([&m, &in]() -> void { ::apsp::io::scan_matrix(in, m); });
 #endif
+
   std::cerr << "Scan: " << scan_ms << "ms" << std::endl;
 
   auto exec_ms = utilz::measure_milliseconds([&m]() -> void { calculate_apsp(m); });
   std::cerr << "Exec: " << exec_ms << "ms" << std::endl;
 
-  auto prnt_ms = utilz::measure_milliseconds([&m]() -> void { ::apsp::io::print_matrix(std::cout, m); });
+  auto prnt_ms = utilz::measure_milliseconds([&m, &out]() -> void { ::apsp::io::print_matrix(out, m); });
   std::cerr << "Prnt: " << prnt_ms << "ms" << std::endl;
 }
