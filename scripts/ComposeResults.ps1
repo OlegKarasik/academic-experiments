417277c8308a14cc383b03d85b8f24e8237a996b
Aleh Karasik <karasik.oleg.nikolaevich@gmail.com>
2021-11-21 18:56:34 +0300

    Improved output.

--- scripts/ComposeResults.ps1
+++ scripts/ComposeResults.ps1
@@ -1,21 +1,21 @@
 ﻿[CmdletBinding()]
 param(
-    [ValidateNotNullOrEmpty()]
-    [string]$TargetDirectory = $(throw '-TargetDirectory parameter is required.'),
-    [ValidateNotNullOrEmpty()]
-    [string]$NamePattern = $(throw '-NamePattern parameter is required.'),
-    [ValidateNotNullOrEmpty()]
-    [string]$GroupPattern = $(throw '-GroupPattern parameter is required.'),
-    [ValidateNotNullOrEmpty()]
-    [string]$DataPattern = $(throw '-DataPattern parameter is required.'),
-    [ValidateNotNullOrEmpty()]
-    [string]$Output = $(throw '-Output parameter is required.'),
-    [switch]$Optional,
-    $LineCount = 500
+  [ValidateNotNullOrEmpty()]
+  [string] $TargetDirectory = $(throw '-TargetDirectory parameter is required.'),
+  [ValidateNotNullOrEmpty()]
+  [string] $NamePattern = $(throw '-NamePattern parameter is required.'),
+  [ValidateNotNullOrEmpty()]
+  [string] $GroupPattern = $(throw '-GroupPattern parameter is required.'),
+  [ValidateNotNullOrEmpty()]
+  [string] $DataPattern = $(throw '-DataPattern parameter is required.'),
+  [ValidateNotNullOrEmpty()]
+  [string] $Output = $(throw '-Output parameter is required.'),
+  [switch] $Optional,
+  $LineCount = 500
 )
 
 if (-not (Test-Path -Path $TargetDirectory -PathType Container -ErrorAction Stop)) {
-    throw "Directory '$TargetDirectory' does not exist";
+  throw "Directory '$TargetDirectory' does not exist";
 };
 
 $Output = Join-Path -Path $TargetDirectory -ChildPath $Output -ErrorAction Stop;
@@ -30,65 +30,66 @@ Write-Verbose -Message "LINE COUNT    : $LineCount" -ErrorAction Stop;
 $Content = @();
 $Sort = { [regex]::Replace($_, '\d+', { $args[0].Value.PadLeft(20) }) }
 try {
-    Write-Host "Processing information from '$TargetDirectory'" -ErrorAction Stop;
-    Get-ChildItem -Path $TargetDirectory -Recurse -Directory -ErrorAction Stop | % {
-        Write-Host "Scanning: '$($_.FullName)'" -ErrorAction Stop;
-        Get-ChildItem -Path $_.FullName -File -ErrorAction Stop | ? { return $_.Name -match $NamePattern; } | Sort-Object $Sort | % {
-            Write-Host "- Composing results from: '$($_.Name)'" -ErrorAction Stop;
-            $Value = $null;
-            $Group = $null;
-            do {
-                Get-Content -Path $_.FullName -ReadCount $LineCount -ErrorAction Stop | % {
-                    if ($Group -eq $null) {
-                        $match = $_ -match $GroupPattern;
-                        if ($match) {
-                            $match[0] -match $GroupPattern | Out-Null;
-                            $Group = $matches[1];
-                        };
-                    };
-                    if ($Value -eq $null) {
-                        $match = $_ -match $DataPattern;
-                        if ($match) {
-                            $match[0] -match $DataPattern | Out-Null;
-                            $Value = $matches[1];
-                        };
-                    };
-                    if (($Group -ne $null) -and ($Value -ne $null)) {
-                        break;
-                    };
-                };
-                break;
-            } while ($true);
-            if (($Group -eq $null) -or ($Value -eq $null)) {
-              if ($Optional -eq $false) {
-                throw 'Pattern search failed with the following error: -GroupPattern or -DataPattern has no result';
-              }
-              return;
+  Write-Host "Processing information from '$TargetDirectory' into '$Output'" -ErrorAction Stop;
+  Get-ChildItem -Path $TargetDirectory -Recurse -Directory -ErrorAction Stop | % {
+    Write-Verbose -Message "Scanning: '$($_.FullName)'" -ErrorAction Stop;
+    Get-ChildItem -Path $_.FullName -File -ErrorAction Stop | ? { return $_.Name -match $NamePattern; } | Sort-Object $Sort | % {
+      Write-Host "- Composing results from: '$($_.Name)'" -ErrorAction Stop;
+      $Value = $null;
+      $Group = $null;
+      do {
+        Get-Content -Path $_.FullName -ReadCount $LineCount -ErrorAction Stop | % {
+          if ($Group -eq $null) {
+            $match = $_ -match $GroupPattern;
+            if ($match) {
+              $match[0] -match $GroupPattern | Out-Null;
+              $Group = $matches[1];
             };
-
-            $Found = $false;
-            for ($i = 0; $i -lt $Content.Length; ++$i) {
-                if ($Content[$i][0] -eq $Group) {
-                    $Found = $true;
-                    $Content[$i] += $Value;
-
-                    return;
-                };
+          };
+          if ($Value -eq $null) {
+            $match = $_ -match $DataPattern;
+            if ($match) {
+              $match[0] -match $DataPattern | Out-Null;
+              $Value = $matches[1];
             };
-            if (-not $Found) {
-                $Content += , @($Group, $Value);
-            };
+          };
+          if (($Group -ne $null) -and ($Value -ne $null)) {
+            break;
+          };
         };
+        break;
+      } while ($true);
+      if (($Group -eq $null) -or ($Value -eq $null)) {
+        if ($Optional -eq $false) {
+          throw 'Pattern search failed with the following error: -GroupPattern or -DataPattern has no result';
+        }
+        return;
+      };
+
+      $Found = $false;
+      for ($i = 0; $i -lt $Content.Length; ++$i) {
+        if ($Content[$i][0] -eq $Group) {
+          $Found = $true;
+          $Content[$i] += $Value;
+
+          return;
+        };
+      };
+      if (-not $Found) {
+        $Content += , @($Group, $Value);
+      };
     };
-    $ContentLine = @();
-    for ($i = 0; $i -lt $Content.Length; ++$i) {
-        $ContentLine += $Content[$i] -join ' ';
-    };
-    $ContentLine = $ContentLine -join $([System.Environment]::NewLine);
-    if ($ContentLine.Length -ne 0) {
-        Set-Content -Path $Output -Value $ContentLine -ErrorAction Stop;
-    };
-} catch {
-    throw $_;
+  };
+  $ContentLine = @();
+  for ($i = 0; $i -lt $Content.Length; ++$i) {
+    $ContentLine += $Content[$i] -join ' ';
+  };
+  $ContentLine = $ContentLine -join $([System.Environment]::NewLine);
+  if ($ContentLine.Length -ne 0) {
+    Set-Content -Path $Output -Value $ContentLine -ErrorAction Stop;
+  };
+}
+catch {
+  throw $_;
 };
 Write-Host "[Done]" -ForegroundColor Green -ErrorAction Stop;
