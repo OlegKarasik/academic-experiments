5add7cff50c566da4c60aa60c3a7b8a7a4b69c11
Oleg Karasik <karasik.oleg.nikolaevich@gmail.com>
2022-05-13 12:23:05 +0300

    Updated CMake (restructured). Fixed benchmark entry point to work with multiple types of algorithms.

--- src/apsp/src/_benchmark.cpp
+++ src/apsp/src/_benchmark.cpp
@@ -14,20 +14,29 @@
 #include "algorithm.hpp"
 #include "io.hpp"
 
-// #ifdef _WIN32
-//   #pragma comment(lib, "Shlwapi.lib")
-// #endif
-
 namespace apsp  = ::apsp;
 namespace utilz = ::utilz;
 
 template<typename T>
 class Fixture : public benchmark::Fixture
 {
-  using matrix    = utilz::square_shape<int>;
-  using matrix_size_type = typename utilz::traits::square_shape_traits<matrix>::size_type;
+// aliasing
+//
+#ifdef APSP_ALG_WIND_UPDOWN
+  using buffer = ::utilz::memory::buffer_dyn;
+#endif
+
+#ifdef APSP_ALG_BLOCKED
+  using matrix = ::utilz::square_shape<::utilz::square_shape<T>>;
+#else
+  using matrix = ::utilz::square_shape<T>;
+#endif
 
 public:
+#ifdef APSP_ALG_WIND_UPDOWN
+  buffer m_buf;
+#endif
+
   matrix m_src;
 
   Fixture()
@@ -39,7 +48,11 @@ public:
     if (!src_fs.is_open())
       throw std::logic_error("erro: the file '" + src_path.generic_string() + "' doesn't exist.");
 
-    apsp::io::scan_matrix(src_fs, false, this->m_src);
+#ifdef APSP_ALG_BLOCKED
+    ::apsp::io::scan_matrix(src_fs, false, this->m_src, 2);
+#else
+    ::apsp::io::scan_matrix(src_fs, false, this->m_src);
+#endif
   }
   ~Fixture()
   {
@@ -49,8 +62,17 @@ public:
 BENCHMARK_TEMPLATE_DEFINE_F(Fixture, ExecuteInt, int)
 (benchmark::State& state)
 {
-  for (auto _ : state)
+  for (auto _ : state) {
+#ifdef APSP_ALG_WIND_UPDOWN
+    auto options = wind_up_apsp(this->m_src, this->m_buf);
+
+    calculate_apsp(this->m_src, options);
+
+    wind_down_apsp(this->m_src, this->m_buf, options);
+#else
     calculate_apsp(this->m_src);
+#endif
+  }
 }
 
 BENCHMARK_REGISTER_F(Fixture, ExecuteInt)->UseRealTime();
