b45ec8859a4c996c882d3fca863aa84e9db85bef
Oleg Karasik <karasik.oleg.nikolaevich@gmail.com>
2022-05-18 15:05:28 +0300

    Replace custom get,set,resize operations with built in.

--- src/tools/src/graphg.cpp
+++ src/tools/src/graphg.cpp
@@ -10,77 +10,6 @@
 #include "graphs-generators.hpp"
 #include "graphs-io.hpp"
 
-template<typename S>
-class set_size
-{
-  static_assert(utilz::traits::square_shape_traits<S>::is::value, "not a square shape");
-
-public:
-  using result_type = typename utilz::traits::square_shape_traits<S>::size_type;
-
-public:
-  void
-  operator()(S& s, typename utilz::traits::square_shape_traits<S>::size_type sz)
-  {
-    s = S(sz);
-  }
-};
-
-template<typename S>
-class set_value
-{
-  static_assert(utilz::traits::square_shape_traits<S>::is::value, "not a square shape");
-
-public:
-  using result_type = typename utilz::traits::square_shape_traits<S>::value_type;
-
-public:
-  void
-  operator()(
-    S&                                                         s,
-    typename utilz::traits::square_shape_traits<S>::size_type  i,
-    typename utilz::traits::square_shape_traits<S>::size_type  j,
-    typename utilz::traits::square_shape_traits<S>::value_type v)
-  {
-    s.at(i, j) = v;
-  }
-};
-
-template<typename S>
-class get_size
-{
-  static_assert(utilz::traits::square_shape_traits<S>::is::value, "not a square shape");
-
-public:
-  using result_type = typename utilz::traits::square_shape_traits<S>::size_type;
-
-public:
-  typename utilz::traits::square_shape_traits<S>::size_type
-  operator()(S& s)
-  {
-    return s.size();
-  }
-};
-
-template<typename S>
-class get_value
-{
-  static_assert(utilz::traits::square_shape_traits<S>::is::value, "not a square shape");
-
-public:
-  using result_type = typename utilz::traits::square_shape_traits<S>::value_type;
-
-public:
-  typename utilz::traits::square_shape_traits<S>::value_type
-  operator()(
-    S&                                                        s,
-    typename utilz::traits::square_shape_traits<S>::size_type i,
-    typename utilz::traits::square_shape_traits<S>::size_type j)
-  {
-    return s.at(i, j);
-  }
-};
-
 int
 main(int argc, char* argv[])
 {
@@ -90,8 +19,8 @@ main(int argc, char* argv[])
   utilz::square_shape<int>                                                                   random_adj;
   std::vector<utilz::graphs::generators::promised_path<utilz::square_shape<int>::size_type>> vec;
 
-  set_size<utilz::square_shape<int>>  s_size;
-  set_value<utilz::square_shape<int>> s_value;
+  utilz::procedures::square_shape_set_size<utilz::square_shape<int>> s_size;
+  utilz::procedures::square_shape_set<utilz::square_shape<int>>      s_value;
   utilz::graphs::generators::random_graph(
     v, e, vec, random_adj, s_size, s_value, utilz::graphs::generators::directed_acyclic_graph_tag());
 
@@ -103,8 +32,8 @@ main(int argc, char* argv[])
       if (random_adj.at(i, j) == 1)
         random_adj.at(i, j) = vertex_distribution(distribution_engine);
 
-  get_size<utilz::square_shape<int>>  g_size;
-  get_value<utilz::square_shape<int>> g_value;
+  utilz::procedures::square_shape_get_size<utilz::square_shape<int>> g_size;
+  utilz::procedures::square_shape_get<utilz::square_shape<int>>      g_value;
 
   std::ofstream output("D:\\myfile.g", std::ios_base::binary);
 
