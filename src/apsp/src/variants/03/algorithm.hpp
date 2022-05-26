f19801ac8c4445245757aebed1c34846043da863
Oleg Karasik <karasik.oleg.nikolaevich@gmail.com>
2022-05-26 23:10:45 +0300

    Implemented windup/windown for 03

--- src/apsp/src/variants/03/algorithm.hpp
+++ src/apsp/src/variants/03/algorithm.hpp
@@ -19,29 +19,32 @@ struct support_arrays
   pointer wrk;
 };
 
-template<typename T, typename A>
+template<typename T, typename A, typename U>
 __attribute__((noinline)) support_arrays<T>
-wind_up_apsp(::utilz::square_shape<T, A>& m, ::utilz::memory::buffer& b)
+wind_up_apsp(::utilz::square_shape<utilz::square_shape<T, A>, U>& blocks, ::utilz::memory::buffer& b)
 {
-  using pointer    = typename ::utilz::traits::square_shape_traits<utilz::square_shape<T, A>>::pointer;
-  using size_type  = typename ::utilz::traits::square_shape_traits<utilz::square_shape<T, A>>::size_type;
-  using value_type = typename ::utilz::traits::square_shape_traits<utilz::square_shape<T, A>>::value_type;
+  using pointer    = typename ::utilz::traits::square_shape_traits<utilz::square_shape<utilz::square_shape<T, A>, U>>::pointer;
+  using size_type  = typename ::utilz::traits::square_shape_traits<utilz::square_shape<utilz::square_shape<T, A>, U>>::size_type;
+  using value_type = typename ::utilz::traits::square_shape_traits<utilz::square_shape<utilz::square_shape<T, A>, U>>::value_type;
 
-  auto allocation_size = m.size() * sizeof(value_type);
+  ::utilz::procedures::square_shape_get_size<utilz::square_shape<utilz::square_shape<T, A>, U>> sz;
+  ::utilz::procedures::square_shape_at<utilz::square_shape<utilz::square_shape<T, A>, U>> at;
+
+  auto allocation_size = sz(blocks) * sizeof(value_type);
 
   support_arrays<T> arrays;
 
-  arrays.mck = reinterpret_cast<pointer>(b.allocate(allocation_size));
-  arrays.drk = reinterpret_cast<pointer>(b.allocate(allocation_size));
-  arrays.mrk = reinterpret_cast<pointer>(b.allocate(allocation_size));
-  arrays.wrk = reinterpret_cast<pointer>(b.allocate(allocation_size));
+  arrays.mck = reinterpret_cast<T*>(b.allocate(allocation_size));
+  arrays.drk = reinterpret_cast<T*>(b.allocate(allocation_size));
+  arrays.mrk = reinterpret_cast<T*>(b.allocate(allocation_size));
+  arrays.wrk = reinterpret_cast<T*>(b.allocate(allocation_size));
 
   // The algorithm requires that all self-loops have non "infinite" value. This
   // doesn't affect correctness of calculations.
   //
-  for (size_type i = size_type(0); i < m.size(); ++i) {
-    if (m.at(i, i) == ::apsp::constants::infinity<value_type>())
-      m.at(i, i) = size_type(0);
+  for (size_type i = size_type(0); i < sz(blocks); ++i) {
+    if (at(blocks, i, i) == ::apsp::constants::infinity<value_type>())
+      at(blocks, i, i) = size_type(0);
 
     arrays.mck[i] = ::apsp::constants::infinity<value_type>();
     arrays.drk[i] = ::apsp::constants::infinity<value_type>();
@@ -52,16 +55,19 @@ wind_up_apsp(::utilz::square_shape<T, A>& m, ::utilz::memory::buffer& b)
   return arrays;
 };
 
-template<typename T, typename A>
+template<typename T, typename A, typename U>
 __attribute__((noinline)) void
-wind_down_apsp(::utilz::square_shape<T, A>& m, ::utilz::memory::buffer& b, support_arrays<T> o)
+wind_down_apsp(::utilz::square_shape<utilz::square_shape<T, A>, U>& blocks, ::utilz::memory::buffer& b, support_arrays<T> o)
 {
-  using size_type  = typename ::utilz::traits::square_shape_traits<utilz::square_shape<T, A>>::size_type;
-  using value_type = typename ::utilz::traits::square_shape_traits<utilz::square_shape<T, A>>::value_type;
+  using size_type  = typename ::utilz::traits::square_shape_traits<utilz::square_shape<utilz::square_shape<T, A>, U>>::size_type;
+  using value_type = typename ::utilz::traits::square_shape_traits<utilz::square_shape<utilz::square_shape<T, A>, U>>::value_type;
 
   using alptr_type = typename ::utilz::memory::buffer::pointer;
 
-  auto allocation_size = m.size() * sizeof(value_type);
+  ::utilz::procedures::square_shape_get_size<utilz::square_shape<utilz::square_shape<T, A>, U>> sz;
+  ::utilz::procedures::square_shape_at<utilz::square_shape<utilz::square_shape<T, A>, U>> at;
+
+  auto allocation_size = sz(blocks) * sizeof(value_type);
 
   b.deallocate(reinterpret_cast<alptr_type>(o.mck), allocation_size);
   b.deallocate(reinterpret_cast<alptr_type>(o.drk), allocation_size);
@@ -71,88 +77,77 @@ wind_down_apsp(::utilz::square_shape<T, A>& m, ::utilz::memory::buffer& b, suppo
   // Restoring the matrix to a state where self-loop is represented as
   // infinity instead of 0.
   //
-  for (size_type i = size_type(0); i < m.size(); ++i)
-    if (m.at(i, i) == size_type(0))
-      m.at(i, i) = ::apsp::constants::infinity<value_type>();
+  for (size_type i = size_type(0); i < sz(blocks); ++i)
+    if (at(blocks, i, i) == size_type(0))
+      at(blocks, i, i) = ::apsp::constants::infinity<value_type>();
 }
 
-template<typename T, typename A>
-void
-calculate_diagonal_block(::utilz::square_shape<T, A>& block, support_arrays<T>& support_arrays)
-{
-  using size_type  = typename ::utilz::traits::square_shape_traits<utilz::square_shape<T, A>>::size_type;
-  using value_type = typename ::utilz::traits::square_shape_traits<utilz::square_shape<T, A>>::value_type;
-  using pointer    = typename ::utilz::traits::square_shape_traits<utilz::square_shape<T, A>>::pointer;
-
-  pointer    pDck;
-  pointer    pDij;
-  value_type minR;
-  value_type sumR, sumC, sumDij;
-  value_type drki;
-  pointer    pBik;
-  pointer    pBki;
-  value_type bki;
-  pointer    pdck, pmrk, pwrk, pmck;
-
-  support_arrays.drk[0] = ::apsp::constants::infinity<value_type>();
-  support_arrays.wrk[0] = block.at(0, 1);
-
-  for (size_type k = size_type(1); k < block.size(); ++k) {
-    pDck = block.at(k - 1);
-    for (size_type i = size_type(0); i < k; ++i)
-      support_arrays.mck[i] = ::apsp::constants::infinity<value_type>();
-
-    pBki = block.at(k);
-    pmrk = &support_arrays.mrk[0];
-    for (size_type i = 0; i < k; ++i, ++pmrk) {
-      minR = ::apsp::constants::infinity<value_type>();
-      bki  = pBki[i];
-      drki = support_arrays.drk[i];
-      pDij = block.at(i);
-      pdck = pDck;
-
-      for (size_type j = 0; j < k; ++j, ++pDij, ++pdck) {
-        sumDij = drki + *pdck;
-        if (*pDij > sumDij)
-          *pDij = sumDij;
-
-        sumR = *pDij + support_arrays.wrk[j];
-        if (minR > sumR)
-          minR = sumR;
-
-        sumC = *pDij + bki;
-        if (support_arrays.mck[j] > sumC)
-          support_arrays.mck[j] = sumC;
-      }
-      *pmrk = minR;
-    }
-
-    pBki = block.at(k);
-    pBik = &block.at(0)[k];
-    pwrk = &support_arrays.wrk[0];
-    pmck = &support_arrays.mck[0];
-
-    for (size_type i = 0; i < k; ++i, ++pBki, pBik += block.size(), ++pwrk, ++pmck) {
-      *pBki = *pmck;
-      *pBik = support_arrays.drk[i] = support_arrays.mrk[i];
-      *pwrk                         = pBik[1];
-    }
-
-    if (k < block.size() - 1)
-      *pwrk = pBik[1];
-  }
-  for (size_type i = size_type(0); i < block.size() - size_type(1); ++i) {
-    drki = support_arrays.drk[i];
-    pDij = block.at(i);
-    pdck = block.at(block.size() - 1);
-
-    for (size_type j = 0; j < block.size() - size_type(1); ++j, ++pDij, ++pdck) {
-      sumDij = drki + *pdck;
-      if (*pDij > sumDij)
-        *pDij = sumDij;
-    }
-  }
-}
+// template<typename T, typename A>
+// void
+// calculate_diagonal_block(::utilz::square_shape<T, A>& block, support_arrays<T>& support_arrays)
+// {
+//   using size_type  = typename ::utilz::traits::square_shape_traits<utilz::square_shape<T, A>>::size_type;
+//   using value_type = typename ::utilz::traits::square_shape_traits<utilz::square_shape<T, A>>::value_type;
+//   using pointer    = typename ::utilz::traits::square_shape_traits<utilz::square_shape<T, A>>::pointer;
+//   pointer    pDck;
+//   pointer    pDij;
+//   value_type minR;
+//   value_type sumR, sumC, sumDij;
+//   value_type drki;
+//   pointer    pBik;
+//   pointer    pBki;
+//   value_type bki;
+//   pointer    pdck, pmrk, pwrk, pmck;
+//   support_arrays.drk[0] = ::apsp::constants::infinity<value_type>();
+//   support_arrays.wrk[0] = block.at(0, 1);
+//   for (size_type k = size_type(1); k < block.size(); ++k) {
+//     pDck = block.at(k - 1);
+//     for (size_type i = size_type(0); i < k; ++i)
+//       support_arrays.mck[i] = ::apsp::constants::infinity<value_type>();
+//     pBki = block.at(k);
+//     pmrk = &support_arrays.mrk[0];
+//     for (size_type i = 0; i < k; ++i, ++pmrk) {
+//       minR = ::apsp::constants::infinity<value_type>();
+//       bki  = pBki[i];
+//       drki = support_arrays.drk[i];
+//       pDij = block.at(i);
+//       pdck = pDck;
+//       for (size_type j = 0; j < k; ++j, ++pDij, ++pdck) {
+//         sumDij = drki + *pdck;
+//         if (*pDij > sumDij)
+//           *pDij = sumDij;
+//         sumR = *pDij + support_arrays.wrk[j];
+//         if (minR > sumR)
+//           minR = sumR;
+//         sumC = *pDij + bki;
+//         if (support_arrays.mck[j] > sumC)
+//           support_arrays.mck[j] = sumC;
+//       }
+//       *pmrk = minR;
+//     }
+//     pBki = block.at(k);
+//     pBik = &block.at(0)[k];
+//     pwrk = &support_arrays.wrk[0];
+//     pmck = &support_arrays.mck[0];
+//     for (size_type i = 0; i < k; ++i, ++pBki, pBik += block.size(), ++pwrk, ++pmck) {
+//       *pBki = *pmck;
+//       *pBik = support_arrays.drk[i] = support_arrays.mrk[i];
+//       *pwrk                         = pBik[1];
+//     }
+//     if (k < block.size() - 1)
+//       *pwrk = pBik[1];
+//   }
+//   for (size_type i = size_type(0); i < block.size() - size_type(1); ++i) {
+//     drki = support_arrays.drk[i];
+//     pDij = block.at(i);
+//     pdck = block.at(block.size() - 1);
+//     for (size_type j = 0; j < block.size() - size_type(1); ++j, ++pDij, ++pdck) {
+//       sumDij = drki + *pdck;
+//       if (*pDij > sumDij)
+//         *pDij = sumDij;
+//     }
+//   }
+// }
 
 // void
 // BCA_C1(int b1, int b3)
@@ -299,7 +294,7 @@ calculate_apsp(::utilz::square_shape<utilz::square_shape<T, A>, U>& blocks, supp
   for (m = 0; m < blocks.size(); ++m) {
     auto& center = blocks.at(m, m);
 
-    calculate_diagonal_block(center);
+    //calculate_diagonal_block(center);
     for (i = 0; i < blocks.size(); ++i) {
       if (i != m) {
         //BCA_C1(i * M + m, m * M + m);
