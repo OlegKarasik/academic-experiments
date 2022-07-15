9998f4823ed0cc5853fd3a476764bd1b70425a25
Oleg Karasik <karasik.oleg.nikolaevich@outlook.com>
2022-07-15 12:44:02 +0300

    Significantely reworked test suite. Now it is parametrised and can detect invalid implementation.

--- src/apsp/src/_test.cpp
+++ src/apsp/src/_test.cpp
@@ -35,6 +35,7 @@ public:
 
   using matrix_at = utilz::procedures::square_shape_at<matrix>;
   using matrix_sz = utilz::procedures::square_shape_get_size<matrix>;
+  using matrix_st = typename utilz::traits::square_shape_traits<matrix>::size_type;
 
 public:
 #ifdef APSP_ALG_HAS_OPTIONS
@@ -44,11 +45,14 @@ public:
   matrix m_src;
   matrix m_res;
 
-  Fixture()
+  Fixture(
+    const std::string& graph_name)
   {
     std::filesystem::path root_path = workspace::root();
-    std::filesystem::path src_path  = root_path / "data/_test/direct-acyclic-graphs/10-14.source.g";
-    std::filesystem::path res_path  = root_path / "data/_test/direct-acyclic-graphs/10-14.result.g";
+    std::filesystem::path data_path = "data/_test/direct-acyclic-graphs";
+
+    std::filesystem::path src_path  = root_path / data_path / (graph_name + ".source.g");
+    std::filesystem::path res_path  = root_path / data_path / (graph_name + ".result.g");
 
     std::ifstream src_fs(src_path);
     if (!src_fs.is_open())
@@ -59,34 +63,55 @@ public:
       throw std::logic_error("erro: the file '" + res_path.generic_string() + "' doesn't exist.");
 
 #ifdef APSP_ALG_HAS_BLOCKS
-    ::apsp::io::scan_matrix(src_fs, false, this->m_src, 2);
-    ::apsp::io::scan_matrix(res_fs, false, this->m_res, 2);
+    ::apsp::io::scan_matrix(src_fs, false, this->m_src, 5);
+    ::apsp::io::scan_matrix(res_fs, false, this->m_res, 5);
 #else
     ::apsp::io::scan_matrix(src_fs, false, this->m_src);
     ::apsp::io::scan_matrix(res_fs, false, this->m_res);
 #endif
   };
   ~Fixture(){};
+
+  void
+  invoke()
+  {
+#ifdef APSP_ALG_HAS_OPTIONS
+    auto options = up(this->m_src, this->m_buf);
+
+    run(this->m_src, options);
+
+    down(this->m_src, this->m_buf, options);
+#else
+    run(this->m_src);
+#endif
+
+    matrix_at at;
+    matrix_sz sz;
+
+    for (auto i = matrix_st(0); i < sz(this->m_src) && sz(this->m_res); ++i)
+      for (auto j = matrix_st(0); j < sz(this->m_src) && sz(this->m_res); ++j)
+        ASSERT_EQ(at(this->m_src, i, j), at(this->m_res, i, j));
+  }
 };
 
 using FixtureT = Fixture<int>;
 
-TEST_F(FixtureT, FIXTURE_NAME)
+class FixtureP
+  : public FixtureT
+  , public ::testing::WithParamInterface<std::string>
 {
-#ifdef APSP_ALG_HAS_OPTIONS
-  auto options = up(this->m_src, this->m_buf);
+public:
+  FixtureP()
+    : FixtureT(GetParam())
+  {
+  }
+};
 
-  run(this->m_src, options);
+const auto values = testing::Values("10-14", "32-376");
 
-  down(this->m_src, this->m_buf, options);
-#else
-  run(this->m_src);
-#endif
+INSTANTIATE_TEST_SUITE_P(FIXTURE_NAME, FixtureP, values);
 
-  matrix_at at;
-  matrix_sz sz;
-
-  for (matrix::size_type i = matrix::size_type(0); i < sz(this->m_src) && sz(this->m_res); ++i)
-    for (matrix::size_type j = matrix::size_type(0); j < sz(this->m_src) && sz(this->m_res); ++j)
-      ASSERT_EQ(at(this->m_src, i, j), at(this->m_res, i, j));
+TEST_P(FixtureP, correctness)
+{
+  this->invoke();
 };
