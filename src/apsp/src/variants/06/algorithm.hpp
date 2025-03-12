64f708ff38dd05ab610226d24566110d02fc414c
OlegKarasik <olegkarasik@coherentsolutions.com>
2025-03-12 22:22:20 +0300

    Moved complex intiialization of reusable objects into up

--- src/apsp/src/variants/06/algorithm.hpp
+++ src/apsp/src/variants/06/algorithm.hpp
@@ -2,40 +2,79 @@
 
 #define APSP_ALG_MATRIX
 
+#define APSP_ALG_EXTRA_CONFIGURATION
+
 #include "portables/hacks/defines.h"
 
+#include "memory.hpp"
 #include "matrix.hpp"
+#include "constants.hpp"
 
 #include "metal-cpp/Metal.hpp"
+
+template<typename T>
+struct run_configuration
+{
+  MTL::Device*               device;
+  MTL::ComputePipelineState* pipeline_state;
+  MTL::CommandQueue*         command_queue;
+};
+
+template<typename T, typename A>
+__hack_noinline void
+up(
+  utilz::matrices::square_matrix<T, A>& m,
+  utilz::memory::buffer& b,
+  run_configuration<T>& run_config)
+{
+  auto library_name  = NS::String::string("_algorithm-v06-metal.metallib", NS::ASCIIStringEncoding);
+  auto function_name = NS::String::string("calculate", NS::ASCIIStringEncoding);
+
+  run_config.device = MTL::CreateSystemDefaultDevice();
+
+  NS::Error*    error   = nullptr;
+  MTL::Library* library = run_config.device->newLibrary(library_name, &error);
+  if (library == nullptr)
+    throw std::runtime_error(
+      "Unable to load \"_algorithm-v06-metal.metallib\" library, please ensure working directory is set to binary location");
+
+  MTL::Function* function = library->newFunction(function_name);
+  if (function == nullptr)
+    throw std::runtime_error(
+      "Unable to load \"calculate\" function from \"_algorithm-v06-metal.metallib\"");
+
+  // Release the library object because it is not needed anymore
+  //
+  library->release();
+
+  run_config.pipeline_state = run_config.device->newComputePipelineState(function, &error);
+
+  // Release the function object because it is not needed anymore
+  //
+  function->release();
+
+  run_config.command_queue = run_config.device->newCommandQueue();
+};
+
+template<typename T, typename A>
+__hack_noinline void
+down(
+  utilz::matrices::square_matrix<T, A>& m,
+  utilz::memory::buffer& b,
+  run_configuration<T>& run_config)
+{
+  run_config.command_queue->release();
+  run_config.pipeline_state->release();
+  run_config.device->release();
+}
 
 template<typename T, typename A>
 __hack_noinline void
 run(
-  ::utilz::matrices::square_matrix<T, A>& m)
+  ::utilz::matrices::square_matrix<T, A>& m,
+  run_configuration<T>& run_config)
 {
-  const unsigned long long arrayLength = 60 * 180 * 100000;
-  const unsigned long long bufferSize = arrayLength * sizeof(float);
-
-  NS::Error *error = nullptr;
-  auto library_name = NS::String::string("_algorithm-v06-metal.metallib", NS::ASCIIStringEncoding);
-
-  MTL::Device *device = MTL::CreateSystemDefaultDevice();
-  MTL::Library *library = device->newLibrary(library_name, &error);
-
-  auto function_name = NS::String::string("calculate", NS::ASCIIStringEncoding);
-  MTL::Function *function = library->newFunction(function_name);
-
-  //
-  library->release();
-  //
-
-  MTL::ComputePipelineState *pipeline_state = device->newComputePipelineState(function, &error);
-
-  //
-  function->release();
-  //
-
-  MTL::Buffer *bufferA = device->newBuffer(m.size() * m.size() * sizeof(T), MTL::ResourceStorageModeShared);
+  MTL::Buffer *bufferA = run_config.device->newBuffer(m.at(0), m.size() * m.size() * sizeof(T), MTL::ResourceStorageModeShared);
   int* content = (int*)bufferA->contents();
   for (auto i = 0; i < m.size(); ++i)
   {
@@ -45,11 +84,8 @@ run(
     }
   }
 
-  ///
-  MTL::CommandQueue *command_queue = device->newCommandQueue();
-
   MTL::Size size = MTL::Size::Make(m.size(), m.size(), 1);
-  NS::UInteger group_size = pipeline_state->maxTotalThreadsPerThreadgroup();
+  NS::UInteger group_size = run_config.pipeline_state->maxTotalThreadsPerThreadgroup();
   if (group_size > m.size())
   {
     group_size = m.size();
@@ -59,10 +95,10 @@ run(
   using size_type = typename ::utilz::matrices::traits::matrix_traits<utilz::matrices::square_matrix<T, A>>::size_type;
   const auto x = m.size();
 
-  MTL::CommandBuffer *command_buffer = command_queue->commandBuffer();
+  MTL::CommandBuffer *command_buffer = run_config.command_queue->commandBuffer();
   MTL::ComputeCommandEncoder *command_encoder = command_buffer->computeCommandEncoder();
 
-  command_encoder->setComputePipelineState(pipeline_state);
+  command_encoder->setComputePipelineState(run_config.pipeline_state);
 
   for (auto k = size_type(0); k < x; ++k) {
     command_encoder->setBuffer(bufferA, 0, 0);
@@ -74,6 +110,8 @@ run(
   command_encoder->endEncoding();
   command_buffer->commit();
   command_buffer->waitUntilCompleted();
+
+  command_encoder->release();
   command_buffer->release();
 
   for (auto i = 0; i < m.size(); ++i)
