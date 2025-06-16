327c1d2788a309eadf44f36fae44eb9c5518082c
Oleg Karasik <karasik.oleg.nikolaevich@gmail.com>
2025-06-16 09:26:08 +0300

    First version of the Cluster Generation algorithm

--- scripts/GenerateClusteredGraph.ps1
+++ scripts/GenerateClusteredGraph.ps1
@@ -1,81 +1,65 @@
-# [CmdletBinding()]
-# param(
-#   [ValidateNotNullOrEmpty()]
-#   [string] $ToolsDirectory = $(throw 'ToolsDirectory parameter is required'),
-#   [ValidateNotNullOrEmpty()]
-#   [string] $OutputFile = $(throw 'OutputFile parameter is required.')
-# )
+[CmdletBinding()]
+param(
+  [ValidateNotNullOrEmpty()]
+  [string] $ClustersConfigPath = $(throw 'ClustersConfigPath parameter is required'),
+  [ValidateNotNullOrEmpty()]
+  [string] $ToolsDirectory = $(throw 'ToolsDirectory parameter is required'),
+  [ValidateNotNullOrEmpty()]
+  [string] $OutputDirectory = $(throw 'OutputDirectory parameter is required.')
+)
 
-$ToolsDirectory = 'C:\Users\Administrator\Projects\GitHub\academic-experiments\src\tools\build\mingw-gcc-release'
-$TempDirectory = "$($ToolsDirectory)"
-
-
-$GraphGeneratorsExecutable = "$($ToolsDirectory)\graphg.exe";
-
-
-
+if (-not (Test-Path -Path $ClustersConfigPath -PathType Leaf -ErrorAction Stop)) {
+  throw "File '$ClustersConfigPath' does not exist";
+};
 if (-not (Test-Path -Path $ToolsDirectory -PathType Container -ErrorAction Stop)) {
   throw "Directory '$ToolsDirectory' does not exist";
 };
+if (-not (Test-Path -Path $OutputDirectory -PathType Container -ErrorAction Stop)) {
+  throw "Directory '$OutputDirectory' does not exist";
+};
 
-$Clusters = @(
-  @{
-    Size = 40;
-    Type = 1; #Complete
-  },
-  @{
-    Size = 50;
-    Type = 1; #Complete
-  },
-  @{
-    Size = 30;
-    Type = 1; #Complete
-  },
-  @{
-    Size = 80;
-    Type = 1; #Complete
-  },
-  @{
-    Size = 20;
-    Type = 1; #Complete
-  }
-);
+$GraphGeneratorsExecutable = "$($ToolsDirectory)\graphg.exe";
 
-# ^
-# Input
+if (-not (Test-Path -Path $GraphGeneratorsExecutable -PathType Leaf -ErrorAction Stop)) {
+  throw "File '$GraphGeneratorsExecutable' does not exist";
+};
+
+$ClustersConfig = Get-Content -Path $ClustersConfigPath -ErrorAction Stop -Raw | ConvertFrom-Json -ErrorAction Stop -AsHashtable;
 
 # Pre-prcoess the input by generating random codes
 # and assigning start and end indexes
 #
 $ClustersCode = 0;
 $ClustersIndex = 0;
-$Clusters | ForEach-Object {
+$ClustersConfig | ForEach-Object {
   $_.Code       = $ClustersCode;
-  $_.StartIndex = $ClustersIndex;
-  $_.EndIndex   = $ClustersIndex + $_.Size;
+  $_.IndexShift = $ClustersIndex;
 
   $ClustersCode  += 1;
   $ClustersIndex += $_.Size;
 };
 
 # Generate cluster.* files based on the input information and
-# read all vertices into global vertices array
+# read all edges into global edges array
 #
-$Index    = 0;
-$Vertices = @();
+$Index = 0;
+$Edges = @();
 
-$Clusters | ForEach-Object {
+$ClustersConfig | ForEach-Object {
   $C = $_;
 
-  $C.VerticesStartIndex = $Vertices.Length;
+  # Save the current length of vertices array
+  # as 'zero' index of the cluster' edges (in the global array).
+  #
+  $C.StartIndex = $Edges.Length;
 
   & $GraphGeneratorsExecutable `
-    -o "$($TempDirectory)\cluster.$($C.Code).g" `
+    -o "$($OutputDirectory)\cluster.$($C.Code).g" `
     -O edgelist `
     -a "$($C.Type)" `
     -v "$($C.Size)"
 
-  Get-Content -Path "$($TempDirectory)\cluster.$($C.Code).g" | ForEach-Object {
+  Get-Content -Path "$($OutputDirectory)\cluster.$($C.Code).g" | ForEach-Object {
     $Success = $_ -match '(?<X>\d+)\s(?<Y>\d+)';
     if ($Success -eq $false) {
       throw 'There is a mistake in the generated graph, the format doesn''t match edgelist';
@@ -84,55 +68,58 @@ $Clusters | ForEach-Object {
     $X = [int]::Parse($Matches['X']) + $Index;
     $Y = [int]::Parse($Matches['Y']) + $Index;
 
-    $Vertices += @{ X = $X; Y = $Y; };
+    $Edges += @{ X = $X; Y = $Y; };
   }
   $Index = $Index + $C.Size;
 
-  $C.VerticesEndIndex = $Vertices.Length;
+  # Save the current length of edges array
+  # as the end index of cluster's edges (in the global array)
+  #
+  $C.EndIndex = $Edges.Length;
 };
 
 # Generate a random connected graph to represent
 # connections between clusters (by generating the connected graph)
 #
 & $GraphGeneratorsExecutable `
-    -o "$($TempDirectory)\cluster.connections.g" `
-    -O edgelist `
-    -a 2 `
-    -v "$($Clusters.Length)" `
-    -e $((($Clusters.Length) * ($Clusters.Length - 1) / 2))
+  -o "$($OutputDirectory)\cluster.connections.g" `
+  -O edgelist `
+  -a 2 `
+  -v "$($ClustersConfig.Length)" `
+  -e $((($ClustersConfig.Length) * ($ClustersConfig.Length - 1) / 2))
 
 # Read connections information and generate additional edges
 # to represents connections between clusters to add them
-# to global vertices array
+# to global edges array
 #
-Get-Content -Path "$($TempDirectory)\cluster.connections.g" | ForEach-Object {
+Get-Content -Path "$($OutputDirectory)\cluster.connections.g" | ForEach-Object {
   $Success = $_ -match '(?<X>\d+)\s(?<Y>\d+)';
   if ($Success -eq $false) {
     throw 'There is a mistake in the generated connections graph, the format doesn''t match edgelist';
   }
 
-  $X = $Clusters[[int]::Parse($Matches['X'])];
-  $Y = $Clusters[[int]::Parse($Matches['Y'])];
+  $X = $ClustersConfig[[int]::Parse($Matches['X'])];
+  $Y = $ClustersConfig[[int]::Parse($Matches['Y'])];
 
-  $XO = [System.Random]::Shared.NextInt64(0, $X.Size - 1) + $X.StartIndex;
-  $YI = [System.Random]::Shared.NextInt64(0, $Y.Size - 1) + $Y.StartIndex;
+  $XO = [System.Random]::Shared.NextInt64(0, $X.Size - 1) + $X.IndexShift;
+  $YI = [System.Random]::Shared.NextInt64(0, $Y.Size - 1) + $Y.IndexShift;
 
-  $Vertices += @{ X = $XO; Y = $YI; }
+  $Edges += @{ X = $XO; Y = $YI; }
 
-  $YO = [System.Random]::Shared.NextInt64(0, $Y.Size - 1) + $Y.StartIndex;
-  $XI = [System.Random]::Shared.NextInt64(0, $X.Size - 1) + $X.StartIndex;
+  $YO = [System.Random]::Shared.NextInt64(0, $Y.Size - 1) + $Y.IndexShift;
+  $XI = [System.Random]::Shared.NextInt64(0, $X.Size - 1) + $X.IndexShift;
 
-  $Vertices += @{ X = $YO; Y = $XI; }
+  $Edges += @{ X = $YO; Y = $XI; }
 }
 
 # Randomise vertex values by performing random replacements
 #
 $RandomIterations = [System.Random]::Shared.NextInt64($Index / 2, $Index);
 for ($i = 0; $i -lt $RandomIterations; ++$i) {
-  $O = [System.Random]::Shared.NextInt64(0, $Index);
-  $R = [System.Random]::Shared.NextInt64(0, $Index);
+  $O = [System.Random]::Shared.NextInt64(0, $Index - 1);
+  $R = [System.Random]::Shared.NextInt64(0, $Index - 1);
 
-  $Vertices | % {
+  $Edges | ForEach-Object {
     if ($_.X -eq $O) { $_.X = $R; } elseif ($_.X -eq $R) { $_.X = $O; }
     if ($_.Y -eq $O) { $_.Y = $R; } elseif ($_.Y -eq $R) { $_.Y = $O; }
   };
@@ -140,27 +127,30 @@ for ($i = 0; $i -lt $RandomIterations; ++$i) {
 
 # Collect clusters information
 #
-$Clusters | ForEach-Object {
-  $Values = @();
+$ClustersConfig | ForEach-Object {
+  $Vertices = @();
 
-  for ($i = $_.VerticesStartIndex; $i -lt $_.VerticesEndIndex; ++$i) {
-    $Values += $Vertices[$i].X;
+  # Iterate over the cluster's edges (from start to end)
+  # and collect 'from' vertices into the array
+  #
+  for ($i = $_.StartIndex; $i -lt $_.EndIndex; ++$i) {
+    $Vertices += $Edges[$i].X;
   }
 
-  $Values = $Values | Sort-Object | Get-Unique;
+  $Vertices = $Vertices | Sort-Object | Get-Unique;
 
-  if ($Values.Length -ne $_.Size) {
+  if ($Vertices.Length -ne $_.Size) {
     throw 'There is a mistake in the collecting information about cluster''s edges';
   }
 
-  $_.Vertices = $Values;
+  $_.Vertices = $Vertices;
 };
 
 # Print results
 #
 $ClustersOutput      = @();
 $ClustersOutputIndex = 1;
-$Clusters | ForEach-Object {
+$ClustersConfig | ForEach-Object {
   $ClustersOutput += "`$``$ClustersOutputIndex``"
 
   $InternalOuterIndex = 1;
@@ -178,13 +168,16 @@ $Clusters | ForEach-Object {
   $ClustersOutputIndex++;
 };
 
-$VerticesOutput = @();
-$Vertices | % {
-  $VerticesOutput += "$($_.X) $($_.Y)"
+$EdgesOutput = @();
+$Edges | ForEach-Object {
+  $EdgesOutput += "$($_.X) $($_.Y)"
 };
 
-Set-Content -Path "$($TempDirectory)\ClustersOutput.g" -Value $ClustersOutput;
-Set-Content -Path "$($TempDirectory)\VerticesOutput.g" -Value $VerticesOutput;
+Set-Content -Path "$($OutputDirectory)\ClustersOutput.g" -Value $ClustersOutput;
+Set-Content -Path "$($OutputDirectory)\EdgesOutput.g" -Value $EdgesOutput;
 
-
+$ClustersConfig | ForEach-Object {
+  Remove-Item -Path "$($OutputDirectory)\cluster.$($_.Code).g";
+}
+"$($OutputDirectory)\cluster.connections.g"
 
