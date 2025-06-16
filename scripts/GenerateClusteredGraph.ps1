b85e563da210acff9096edefcc28af4d68d2441a
Oleg Karasik <karasik.oleg.nikolaevich@gmail.com>
2025-06-16 23:40:58 +0300

    Implemented auto detection of tools. Implemented generation of graph in the weightlist format.

--- scripts/GenerateClusteredGraph.ps1
+++ scripts/GenerateClusteredGraph.ps1
@@ -1,27 +1,53 @@
 [CmdletBinding()]
 param(
   [ValidateNotNullOrEmpty()]
-  [string] $ClustersConfigPath = $(throw 'ClustersConfigPath parameter is required'),
+  [string] $ClustersConfigPath  = $(throw './clusters.clusters-config'),
   [ValidateNotNullOrEmpty()]
-  [string] $ToolsDirectory = $(throw 'ToolsDirectory parameter is required'),
+  [string] $ConnectionEdgeCount = $(throw '-ConnectionEdgeCount parameter is required'),
   [ValidateNotNullOrEmpty()]
-  [string] $OutputDirectory = $(throw 'OutputDirectory parameter is required.')
+  [string] $OutputDirectory     = $(throw '-OutputDirectory parameter is required'),
+  [string] $ToolsDirectory      = ''
 )
 
 if (-not (Test-Path -Path $ClustersConfigPath -PathType Leaf -ErrorAction Stop)) {
   throw "File '$ClustersConfigPath' does not exist";
 };
-if (-not (Test-Path -Path $ToolsDirectory -PathType Container -ErrorAction Stop)) {
-  throw "Directory '$ToolsDirectory' does not exist";
-};
 if (-not (Test-Path -Path $OutputDirectory -PathType Container -ErrorAction Stop)) {
   throw "Directory '$OutputDirectory' does not exist";
 };
 
-$GraphGeneratorsExecutable = "$($ToolsDirectory)\graphg.exe";
+$GraphGeneratorsExecutable = $null;
+$GraphConvertersExecutable = $null;
+$GraphAnalysersExecutable  = $null;
 
+if ($ToolsDirectory -eq '') {
+  $Paths = @(
+    './../src/tools/build/mingw-gcc-release',
+    './../src/tools/build/osx-llvm-clang-release'
+  );
+
+  for ($i = 0; $i -lt $Paths.Length; ++$i) {
+    $ToolsDirectory            = Join-Path -Path $PSScriptRoot -ChildPath './../src/tools/build/mingw-gcc-release' -Resolve;
+    $GraphGeneratorsExecutable = Join-Path -Path $ToolsDirectory -ChildPath 'graphg.exe';
+    $GraphConvertersExecutable = Join-Path -Path $ToolsDirectory -ChildPath 'graphc.exe';
+    $GraphAnalysersExecutable  = Join-Path -Path $ToolsDirectory -ChildPath 'grapha.exe';
+
+    if ((Test-Path -Path $GraphGeneratorsExecutable -PathType Leaf -ErrorAction Stop) -and
+        (Test-Path -Path $GraphConvertersExecutable -PathType Leaf -ErrorAction Stop) -and
+        (Test-Path -Path $GraphAnalysersExecutable  -PathType Leaf -ErrorAction Stop)) {
+
+      break;
+    };
+  }
+}
 if (-not (Test-Path -Path $GraphGeneratorsExecutable -PathType Leaf -ErrorAction Stop)) {
-  throw "File '$GraphGeneratorsExecutable' does not exist";
+  throw "Tool '$GraphGeneratorsExecutable' does not exist";
+};
+if (-not (Test-Path -Path $GraphConvertersExecutable -PathType Leaf -ErrorAction Stop)) {
+  throw "Tool '$GraphConvertersExecutable' does not exist";
+};
+if (-not (Test-Path -Path $GraphAnalysersExecutable -PathType Leaf -ErrorAction Stop)) {
+  throw "Tool '$GraphAnalysersExecutable' does not exist";
 };
 
 $ClustersConfig = Get-Content -Path $ClustersConfigPath -ErrorAction Stop -Raw | ConvertFrom-Json -ErrorAction Stop -AsHashtable;
@@ -54,12 +80,12 @@ $ClustersConfig | ForEach-Object {
   $C.StartIndex = $Edges.Length;
 
   & $GraphGeneratorsExecutable `
-    -o "$($OutputDirectory)\cluster.$($C.Code).g" `
+    -o "$($OutputDirectory)\intermediate.$($C.Code).g" `
     -O edgelist `
     -a "$($C.Type)" `
     -v "$($C.Size)"
 
-  Get-Content -Path "$($OutputDirectory)\cluster.$($C.Code).g" | ForEach-Object {
+  Get-Content -Path "$($OutputDirectory)\intermediate.$($C.Code).g" | ForEach-Object {
     $Success = $_ -match '(?<X>\d+)\s(?<Y>\d+)';
     if ($Success -eq $false) {
       throw 'There is a mistake in the generated graph, the format doesn''t match edgelist';
@@ -82,17 +108,17 @@ $ClustersConfig | ForEach-Object {
 # connections between clusters (by generating the connected graph)
 #
 & $GraphGeneratorsExecutable `
-  -o "$($OutputDirectory)\cluster.connections.g" `
+  -o "$($OutputDirectory)\intermediate.connections.g" `
   -O edgelist `
   -a 2 `
   -v "$($ClustersConfig.Length)" `
-  -e $((($ClustersConfig.Length) * ($ClustersConfig.Length - 1) / 2))
+  -e $ConnectionEdgeCount
 
 # Read connections information and generate additional edges
 # to represents connections between clusters to add them
 # to global edges array
 #
-Get-Content -Path "$($OutputDirectory)\cluster.connections.g" | ForEach-Object {
+Get-Content -Path "$($OutputDirectory)\intermediate.connections.g" | ForEach-Object {
   $Success = $_ -match '(?<X>\d+)\s(?<Y>\d+)';
   if ($Success -eq $false) {
     throw 'There is a mistake in the generated connections graph, the format doesn''t match edgelist';
@@ -146,7 +172,16 @@ $ClustersConfig | ForEach-Object {
   $_.Vertices = $Vertices;
 };
 
-# Print results
+# Print Edges
+#
+$EdgesOutput = @();
+$Edges | ForEach-Object {
+  $W = [System.Random]::Shared.NextInt64(1, 1000);
+  $EdgesOutput += "$($_.X) $($_.Y) $W"
+};
+Set-Content -Path "$($OutputDirectory)\output.g" -Value $EdgesOutput;
+
+# Print Clusters
 #
 $ClustersOutput      = @();
 $ClustersOutputIndex = 1;
@@ -167,17 +202,18 @@ $ClustersConfig | ForEach-Object {
 
   $ClustersOutputIndex++;
 };
+Set-Content -Path "$($OutputDirectory)\output.communities.g" -Value $ClustersOutput;
 
-$EdgesOutput = @();
-$Edges | ForEach-Object {
-  $EdgesOutput += "$($_.X) $($_.Y)"
-};
-
-Set-Content -Path "$($OutputDirectory)\ClustersOutput.g" -Value $ClustersOutput;
-Set-Content -Path "$($OutputDirectory)\EdgesOutput.g" -Value $EdgesOutput;
+# Perform analysis
+#
+& $GraphAnalysersExecutable `
+  -o "$($OutputDirectory)\output.analysis.g" `
+  -g "$($OutputDirectory)\output.g" `
+  -G weightlist `
+  -c "$($OutputDirectory)\output.communities.g" `
+  -C rlang
 
 $ClustersConfig | ForEach-Object {
-  Remove-Item -Path "$($OutputDirectory)\cluster.$($_.Code).g";
+  Remove-Item -Path "$($OutputDirectory)\intermediate.$($_.Code).g";
 }
-"$($OutputDirectory)\cluster.connections.g"
 
