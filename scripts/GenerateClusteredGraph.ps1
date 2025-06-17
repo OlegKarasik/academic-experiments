aff03d7500a01b9bce4ab81d7e6a51ee487f8cff
Oleg Karasik <karasik.oleg.nikolaevich@gmail.com>
2025-06-18 00:52:28 +0300

    Significantely improved performance of the graph generation script

--- scripts/GenerateClusteredGraph.ps1
+++ scripts/GenerateClusteredGraph.ps1
@@ -1,19 +1,30 @@
 [CmdletBinding()]
 param(
   [ValidateNotNullOrEmpty()]
-  [string] $ClustersConfigPath  = $(throw './clusters.clusters-config'),
+  [string] $ClustersConfigPath       = $(throw './clusters.clusters-config'),
   [ValidateNotNullOrEmpty()]
-  [string] $ConnectionEdgeCount = $(throw '-ConnectionEdgeCount parameter is required'),
+  [string] $ConnectionEdgePercentage = $(throw '-ConnectionEdgePercentage parameter is required'),
   [ValidateNotNullOrEmpty()]
-  [string] $OutputDirectory     = $(throw '-OutputDirectory parameter is required'),
-  [string] $ToolsDirectory      = ''
+  [string] $OutputDirectory          = $(throw '-OutputDirectory parameter is required'),
+  [string] $ToolsDirectory           = '',
+  [int]    $MinWeight                = 1,
+  [int]    $MaxWeight                = 100
 )
+
+Write-Verbose -Message "CLUSTERS CONFIG PATH         : $ClustersConfigPath" -ErrorAction Stop;
+Write-Verbose -Message "OUTPUT DIRECTORY             : $OutputDirectory" -ErrorAction Stop;
+Write-Verbose -Message "CONNECTION EDGE COUNT        : $ConnectionEdgeCount" -ErrorAction Stop;
+Write-Verbose -Message "MIN WEIGHT                   : $MinWeight" -ErrorAction Stop;
+Write-Verbose -Message "MAX WEIGHT                   : $MaxWeight" -ErrorAction Stop;
 
 if (-not (Test-Path -Path $ClustersConfigPath -PathType Leaf -ErrorAction Stop)) {
   throw "File '$ClustersConfigPath' does not exist";
 };
 if (-not (Test-Path -Path $OutputDirectory -PathType Container -ErrorAction Stop)) {
   throw "Directory '$OutputDirectory' does not exist";
+};
+if ($MinWeight -eq 0) {
+  throw "-MinWeight parameter can't be 0";
 };
 
 $GraphGeneratorsExecutable = $null;
@@ -50,6 +61,8 @@ if (-not (Test-Path -Path $GraphAnalysersExecutable -PathType Leaf -ErrorAction 
   throw "Tool '$GraphAnalysersExecutable' does not exist";
 };
 
+Write-Verbose -Message "TOOLS DIRECTORY              : $ToolsDirectory" -ErrorAction Stop;
+
 $ClustersConfig = Get-Content -Path $ClustersConfigPath -ErrorAction Stop -Raw | ConvertFrom-Json -ErrorAction Stop -AsHashtable;
 
 # Pre-prcoess the input by generating random codes
@@ -83,9 +96,10 @@ $ClustersConfig | ForEach-Object {
     -o "$($OutputDirectory)\intermediate.$($C.Code).g" `
     -O edgelist `
     -a "$($C.Type)" `
-    -v "$($C.Size)"
+    -v "$($C.Size)" `
+    2> $null 1> $null;
 
-  Get-Content -Path "$($OutputDirectory)\intermediate.$($C.Code).g" | ForEach-Object {
+  $ClusterEdges = Get-Content -Path "$($OutputDirectory)\intermediate.$($C.Code).g" | ForEach-Object {
     $Success = $_ -match '(?<X>\d+)\s(?<Y>\d+)';
     if ($Success -eq $false) {
       throw 'There is a mistake in the generated graph, the format doesn''t match edgelist';
@@ -94,8 +108,10 @@ $ClustersConfig | ForEach-Object {
     $X = [int]::Parse($Matches['X']) + $Index;
     $Y = [int]::Parse($Matches['Y']) + $Index;
 
-    $Edges += @{ X = $X; Y = $Y; };
+    return @{ X = $X; Y = $Y; };
   }
+  $Edges = $Edges + $ClusterEdges;
+
   $Index = $Index + $C.Size;
 
   # Save the current length of edges array
@@ -112,13 +128,13 @@ $ClustersConfig | ForEach-Object {
   -O edgelist `
   -a 2 `
   -v "$($ClustersConfig.Length)" `
-  -e $ConnectionEdgeCount
+  -e $ConnectionEdgePercentage;
 
 # Read connections information and generate additional edges
 # to represents connections between clusters to add them
 # to global edges array
 #
-Get-Content -Path "$($OutputDirectory)\intermediate.connections.g" | ForEach-Object {
+$ConnectionEdges = Get-Content -Path "$($OutputDirectory)\intermediate.connections.g" | ForEach-Object {
   $Success = $_ -match '(?<X>\d+)\s(?<Y>\d+)';
   if ($Success -eq $false) {
     throw 'There is a mistake in the generated connections graph, the format doesn''t match edgelist';
@@ -127,43 +143,55 @@ Get-Content -Path "$($OutputDirectory)\intermediate.connections.g" | ForEach-Obj
   $X = $ClustersConfig[[int]::Parse($Matches['X'])];
   $Y = $ClustersConfig[[int]::Parse($Matches['Y'])];
 
-  $XO = [System.Random]::Shared.NextInt64(0, $X.Size - 1) + $X.IndexShift;
-  $YI = [System.Random]::Shared.NextInt64(0, $Y.Size - 1) + $Y.IndexShift;
+  $XO = [System.Random]::Shared.NextInt64(0, $X.Size) + $X.IndexShift;
+  $YI = [System.Random]::Shared.NextInt64(0, $Y.Size) + $Y.IndexShift;
 
-  $Edges += @{ X = $XO; Y = $YI; }
-
-  $YO = [System.Random]::Shared.NextInt64(0, $Y.Size - 1) + $Y.IndexShift;
-  $XI = [System.Random]::Shared.NextInt64(0, $X.Size - 1) + $X.IndexShift;
-
-  $Edges += @{ X = $YO; Y = $XI; }
+  return @{ X = $XO; Y = $YI; }
 }
+$Edges = $Edges + $ConnectionEdges;
 
 # Randomise vertex values by performing random replacements
 #
+$ShuffleSet = New-Object System.Collections.Generic.HashSet[int];
 $RandomIterations = [System.Random]::Shared.NextInt64($Index / 2, $Index);
 for ($i = 0; $i -lt $RandomIterations; ++$i) {
-  $O = [System.Random]::Shared.NextInt64(0, $Index - 1);
-  $R = [System.Random]::Shared.NextInt64(0, $Index - 1);
+  $O = [System.Random]::Shared.NextInt64(0, $Index);
+  $R = [System.Random]::Shared.NextInt64(0, $Index);
 
-  $Edges | ForEach-Object {
-    if ($_.X -eq $O) { $_.X = $R; } elseif ($_.X -eq $R) { $_.X = $O; }
-    if ($_.Y -eq $O) { $_.Y = $R; } elseif ($_.Y -eq $R) { $_.Y = $O; }
-  };
+  if ($ShuffleSet.Add($O) -eq $false) {
+    continue;
+  }
+  if ($ShuffleSet.Add($R) -eq $false) {
+    continue;
+  }
+
+  $X = $ClustersConfig | ? { ($_.IndexShift -le $O) -and (($_.IndexShift + $_.Size) -ge $O) } | Select-Object -First 1;
+  $Y = $ClustersConfig | ? { ($_.IndexShift -le $R) -and (($_.IndexShift + $_.Size) -ge $R) } | Select-Object -First 1;
+
+  for ($j = $X.StartIndex; $j -lt $X.EndIndex; ++$j) {
+    if ($Edges[$j].X -eq $O) { $Edges[$j].X = $R; } elseif ($Edges[$j].X -eq $R) { $Edges[$j].X = $O; }
+    if ($Edges[$j].Y -eq $O) { $Edges[$j].Y = $R; } elseif ($Edges[$j].Y -eq $R) { $Edges[$j].Y = $O; }
+  }
+  for ($j = $Y.StartIndex; $j -lt $Y.EndIndex; ++$j) {
+    if ($Edges[$j].X -eq $O) { $Edges[$j].X = $R; } elseif ($Edges[$j].X -eq $R) { $Edges[$j].X = $O; }
+    if ($Edges[$j].Y -eq $O) { $Edges[$j].Y = $R; } elseif ($Edges[$j].Y -eq $R) { $Edges[$j].Y = $O; }
+  }
+  for ($j = ($Edges.Length - $ConnectionEdges.Length - 1); $j -lt $Edges.Length; ++$j) {
+    if ($Edges[$j].X -eq $O) { $Edges[$j].X = $R; } elseif ($Edges[$j].X -eq $R) { $Edges[$j].X = $O; }
+    if ($Edges[$j].Y -eq $O) { $Edges[$j].Y = $R; } elseif ($Edges[$j].Y -eq $R) { $Edges[$j].Y = $O; }
+  }
 }
 
 # Collect clusters information
 #
 $ClustersConfig | ForEach-Object {
-  $Vertices = @();
-
   # Iterate over the cluster's edges (from start to end)
   # and collect 'from' vertices into the array
   #
-  for ($i = $_.StartIndex; $i -lt $_.EndIndex; ++$i) {
-    $Vertices += $Edges[$i].X;
-  }
-
-  $Vertices = $Vertices | Sort-Object | Get-Unique;
+  $Vertices = $_.StartIndex .. ($_.EndIndex - 1)
+            | ForEach-Object { return $Edges[$_].X; }
+            | Sort-Object
+            | Get-Unique;
 
   if ($Vertices.Length -ne $_.Size) {
     throw 'There is a mistake in the collecting information about cluster''s edges';
@@ -174,10 +202,9 @@ $ClustersConfig | ForEach-Object {
 
 # Print Edges
 #
-$EdgesOutput = @();
-$Edges | ForEach-Object {
-  $W = [System.Random]::Shared.NextInt64(1, 1000);
-  $EdgesOutput += "$($_.X) $($_.Y) $W"
+$EdgesOutput = $Edges | ForEach-Object {
+  $W = [System.Random]::Shared.NextInt64($MinWeight, $MaxWeight);
+  return "$($_.X) $($_.Y) $W"
 };
 Set-Content -Path "$($OutputDirectory)\output.g" -Value $EdgesOutput;
 
@@ -211,9 +238,8 @@ Set-Content -Path "$($OutputDirectory)\output.communities.g" -Value $ClustersOut
   -g "$($OutputDirectory)\output.g" `
   -G weightlist `
   -c "$($OutputDirectory)\output.communities.g" `
-  -C rlang
+  -C rlang;
 
 $ClustersConfig | ForEach-Object {
   Remove-Item -Path "$($OutputDirectory)\intermediate.$($_.Code).g";
 }
-
