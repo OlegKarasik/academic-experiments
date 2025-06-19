5fcff3e03fd218db5563252b3d7f3d53204bf5c5
Oleg Karasik <karasik.oleg.nikolaevich@gmail.com>
2025-06-19 21:22:07 +0300

    Fixed an issue when shuffle damaged the resulting graph

--- scripts/GenerateClusteredGraph.ps1
+++ scripts/GenerateClusteredGraph.ps1
@@ -13,7 +13,7 @@ param(
 
 Write-Verbose -Message "CLUSTERS CONFIG PATH         : $ClustersConfigPath" -ErrorAction Stop;
 Write-Verbose -Message "OUTPUT DIRECTORY             : $OutputDirectory" -ErrorAction Stop;
-Write-Verbose -Message "CONNECTION EDGE COUNT        : $ConnectionEdgeCount" -ErrorAction Stop;
+Write-Verbose -Message "CONNECTION EDGE PERCENTAGE   : $ConnectionEdgePercentage" -ErrorAction Stop;
 Write-Verbose -Message "MIN WEIGHT                   : $MinWeight" -ErrorAction Stop;
 Write-Verbose -Message "MAX WEIGHT                   : $MaxWeight" -ErrorAction Stop;
 
@@ -84,103 +84,139 @@ $ClustersConfig | ForEach-Object {
 $Index = 0;
 $Edges = @();
 
-$ClustersConfig | ForEach-Object {
-  $C = $_;
+$MeasureClustersGeneration = Measure-Command {
+  $ClustersConfig | ForEach-Object {
+    $C = $_;
 
-  # Save the current length of vertices array
-  # as 'zero' index of the cluster' edges (in the global array).
-  #
-  $C.StartIndex = $Edges.Length;
+    # Save the current length of vertices array
+    # as 'zero' index of the cluster' edges (in the global array).
+    #
+    $C.StartIndex = $Edges.Length;
 
-  & $GraphGeneratorsExecutable `
-    -o "$($OutputDirectory)\intermediate.$($C.Code).g" `
-    -O edgelist `
-    -a "$($C.Type)" `
-    -v "$($C.Size)" `
-    2> $null 1> $null;
+    & $GraphGeneratorsExecutable `
+      -o "$($OutputDirectory)\intermediate.$($C.Code).g" `
+      -O edgelist `
+      -a "$($C.Type)" `
+      -v "$($C.Size)" `
+      2> $null 1> $null;
 
-  $ClusterEdges = Get-Content -Path "$($OutputDirectory)\intermediate.$($C.Code).g" | ForEach-Object {
-    $Success = $_ -match '(?<X>\d+)\s(?<Y>\d+)';
-    if ($Success -eq $false) {
-      throw 'There is a mistake in the generated graph, the format doesn''t match edgelist';
+    $ClusterEdges = Get-Content -Path "$($OutputDirectory)\intermediate.$($C.Code).g" | ForEach-Object {
+      $Success = $_ -match '(?<X>\d+)\s(?<Y>\d+)';
+      if ($Success -eq $false) {
+        throw 'There is a mistake in the generated graph, the format doesn''t match edgelist';
+      }
+
+      $X = [int]::Parse($Matches['X']) + $Index;
+      $Y = [int]::Parse($Matches['Y']) + $Index;
+
+      return @{ X = $X; Y = $Y; };
     }
+    $Edges = $Edges + $ClusterEdges;
 
-    $X = [int]::Parse($Matches['X']) + $Index;
-    $Y = [int]::Parse($Matches['Y']) + $Index;
+    $Index = $Index + $C.Size;
 
-    return @{ X = $X; Y = $Y; };
-  }
-  $Edges = $Edges + $ClusterEdges;
-
-  $Index = $Index + $C.Size;
-
-  # Save the current length of edges array
-  # as the end index of cluster's edges (in the global array)
-  #
-  $C.EndIndex = $Edges.Length;
+    # Save the current length of edges array
+    # as the end index of cluster's edges (in the global array)
+    #
+    $C.EndIndex = $Edges.Length;
+  };
 };
+Write-Verbose "[Clusters Generation Completed] $($MeasureClustersGeneration.TotalSeconds)";
+
+$EdgesIntegrity = $Edges | ForEach-Object { return $_.X } | Sort-Object | Get-Unique;
+if ($EdgesIntegrity.Length -ne $Index) {
+  Write-Verbose "$($EdgesIntegrity.Length) -> $Index"
+
+  throw 'There is a mistake in the edges generation. The number of unique vertices doesn''t match';
+}
+Write-Verbose "[State Verified]";
 
 # Generate a random connected graph to represent
 # connections between clusters (by generating the connected graph)
 #
-& $GraphGeneratorsExecutable `
-  -o "$($OutputDirectory)\intermediate.connections.g" `
-  -O edgelist `
-  -a 2 `
-  -v "$($ClustersConfig.Length)" `
-  -e $ConnectionEdgePercentage;
+$MeasureConnectionGeneration = Measure-Command {
+  & $GraphGeneratorsExecutable `
+    -o "$($OutputDirectory)\intermediate.connections.g" `
+    -O edgelist `
+    -a 2 `
+    -v "$($ClustersConfig.Length)" `
+    -e $ConnectionEdgePercentage `
+    2> $null 1> $null;
 
-# Read connections information and generate additional edges
-# to represents connections between clusters to add them
-# to global edges array
-#
-$ConnectionEdges = Get-Content -Path "$($OutputDirectory)\intermediate.connections.g" | ForEach-Object {
-  $Success = $_ -match '(?<X>\d+)\s(?<Y>\d+)';
-  if ($Success -eq $false) {
-    throw 'There is a mistake in the generated connections graph, the format doesn''t match edgelist';
+  # Read connections information and generate additional edges
+  # to represents connections between clusters to add them
+  # to global edges array
+  #
+
+  $ConnectionEdges = Get-Content -Path "$($OutputDirectory)\intermediate.connections.g" | ForEach-Object {
+    $Success = $_ -match '(?<X>\d+)\s(?<Y>\d+)';
+    if ($Success -eq $false) {
+      throw 'There is a mistake in the generated connections graph, the format doesn''t match edgelist';
+    }
+
+    $X = $ClustersConfig[[int]::Parse($Matches['X'])];
+    $Y = $ClustersConfig[[int]::Parse($Matches['Y'])];
+
+    $XO = [System.Random]::Shared.NextInt64(0, $X.Size) + $X.IndexShift;
+    $YI = [System.Random]::Shared.NextInt64(0, $Y.Size) + $Y.IndexShift;
+
+    return @{ X = $XO; Y = $YI; }
   }
+  $Edges = $Edges + $ConnectionEdges;
+};
+Write-Verbose "[Connection Generation Completed] $($MeasureConnectionGeneration.TotalSeconds)";
 
-  $X = $ClustersConfig[[int]::Parse($Matches['X'])];
-  $Y = $ClustersConfig[[int]::Parse($Matches['Y'])];
+$EdgesIntegrity = $Edges | ForEach-Object { return $_.X } | Sort-Object | Get-Unique;
+if ($EdgesIntegrity.Length -ne $Index) {
+  Write-Verbose "$($EdgesIntegrity.Length) -> $Index"
 
-  $XO = [System.Random]::Shared.NextInt64(0, $X.Size) + $X.IndexShift;
-  $YI = [System.Random]::Shared.NextInt64(0, $Y.Size) + $Y.IndexShift;
-
-  return @{ X = $XO; Y = $YI; }
+  throw 'There is a mistake in the edges generation. The number of unique vertices doesn''t match';
 }
-$Edges = $Edges + $ConnectionEdges;
+Write-Verbose "[State Verified]";
 
 # Randomise vertex values by performing random replacements
 #
-$ShuffleSet = New-Object System.Collections.Generic.HashSet[int];
-$RandomIterations = [System.Random]::Shared.NextInt64($Index / 2, $Index);
-for ($i = 0; $i -lt $RandomIterations; ++$i) {
-  $O = [System.Random]::Shared.NextInt64(0, $Index);
-  $R = [System.Random]::Shared.NextInt64(0, $Index);
+$MeasureShuffle = Measure-Command {
+  $ShuffleSet = New-Object System.Collections.Generic.HashSet[int];
+  $RandomIterations = $Index#[System.Random]::Shared.NextInt64($Index / 2, $Index);
+  for ($i = 0; $i -lt $RandomIterations; ++$i) {
+    $O = [System.Random]::Shared.NextInt64(0, $Index);
+    $R = [System.Random]::Shared.NextInt64(0, $Index);
 
-  if ($ShuffleSet.Add($O) -eq $false) {
-    continue;
-  }
-  if ($ShuffleSet.Add($R) -eq $false) {
-    continue;
-  }
+    if ($O -eq $R) {
+      continue;
+    };
 
-  $X = $ClustersConfig | ? { ($_.IndexShift -le $O) -and (($_.IndexShift + $_.Size) -ge $O) } | Select-Object -First 1;
-  $Y = $ClustersConfig | ? { ($_.IndexShift -le $R) -and (($_.IndexShift + $_.Size) -ge $R) } | Select-Object -First 1;
+    if (($ShuffleSet.Add($O) -eq $false) -or ($ShuffleSet.Add($R) -eq $false)) {
+      continue;
+    }
 
-  for ($j = $X.StartIndex; $j -lt $X.EndIndex; ++$j) {
-    if ($Edges[$j].X -eq $O) { $Edges[$j].X = $R; } elseif ($Edges[$j].X -eq $R) { $Edges[$j].X = $O; }
-    if ($Edges[$j].Y -eq $O) { $Edges[$j].Y = $R; } elseif ($Edges[$j].Y -eq $R) { $Edges[$j].Y = $O; }
-  }
-  for ($j = $Y.StartIndex; $j -lt $Y.EndIndex; ++$j) {
-    if ($Edges[$j].X -eq $O) { $Edges[$j].X = $R; } elseif ($Edges[$j].X -eq $R) { $Edges[$j].X = $O; }
-    if ($Edges[$j].Y -eq $O) { $Edges[$j].Y = $R; } elseif ($Edges[$j].Y -eq $R) { $Edges[$j].Y = $O; }
-  }
-  for ($j = ($Edges.Length - $ConnectionEdges.Length - 1); $j -lt $Edges.Length; ++$j) {
-    if ($Edges[$j].X -eq $O) { $Edges[$j].X = $R; } elseif ($Edges[$j].X -eq $R) { $Edges[$j].X = $O; }
-    if ($Edges[$j].Y -eq $O) { $Edges[$j].Y = $R; } elseif ($Edges[$j].Y -eq $R) { $Edges[$j].Y = $O; }
+    $X = $ClustersConfig | Where-Object { ($_.IndexShift -le $O) -and (($_.IndexShift + $_.Size) -gt $O) } | Select-Object -First 1;
+    $Y = $ClustersConfig | Where-Object { ($_.IndexShift -le $R) -and (($_.IndexShift + $_.Size) -gt $R) } | Select-Object -First 1;
+
+    for ($j = $X.StartIndex; $j -lt $X.EndIndex; ++$j) {
+      if ($Edges[$j].X -eq $O) { $Edges[$j].X = $R; } elseif ($Edges[$j].X -eq $R) { $Edges[$j].X = $O; }
+      if ($Edges[$j].Y -eq $O) { $Edges[$j].Y = $R; } elseif ($Edges[$j].Y -eq $R) { $Edges[$j].Y = $O; }
+    }
+    for ($j = $Y.StartIndex; $j -lt $Y.EndIndex; ++$j) {
+      if ($Edges[$j].X -eq $O) { $Edges[$j].X = $R; } elseif ($Edges[$j].X -eq $R) { $Edges[$j].X = $O; }
+      if ($Edges[$j].Y -eq $O) { $Edges[$j].Y = $R; } elseif ($Edges[$j].Y -eq $R) { $Edges[$j].Y = $O; }
+    }
+    for ($j = ($Edges.Length - $ConnectionEdges.Length); $j -lt $Edges.Length; ++$j) {
+      if ($Edges[$j].X -eq $O) { $Edges[$j].X = $R; } elseif ($Edges[$j].X -eq $R) { $Edges[$j].X = $O; }
+      if ($Edges[$j].Y -eq $O) { $Edges[$j].Y = $R; } elseif ($Edges[$j].Y -eq $R) { $Edges[$j].Y = $O; }
+    }
   }
 }
+Write-Verbose "[Shuffle Completed] $($MeasureShuffle.TotalSeconds)";
+
+$EdgesIntegrity = $Edges | ForEach-Object { return $_.X } | Sort-Object | Get-Unique;
+if ($EdgesIntegrity.Length -ne $Index) {
+  Write-Verbose "$($EdgesIntegrity.Length) -> $Index"
+
+  throw 'There is a mistake in the edges generation. The number of unique vertices doesn''t match';
+}
+Write-Verbose "[State Verified]";
 
 # Collect clusters information
 #
@@ -233,12 +269,16 @@ Set-Content -Path "$($OutputDirectory)\output.communities.g" -Value $ClustersOut
 
 # Perform analysis
 #
-& $GraphAnalysersExecutable `
-  -o "$($OutputDirectory)\output.analysis.g" `
-  -g "$($OutputDirectory)\output.g" `
-  -G weightlist `
-  -c "$($OutputDirectory)\output.communities.g" `
-  -C rlang;
+$MeasureAnalysis = Measure-Command {
+  & $GraphAnalysersExecutable `
+    -o "$($OutputDirectory)\output.analysis.g" `
+    -g "$($OutputDirectory)\output.g" `
+    -G weightlist `
+    -c "$($OutputDirectory)\output.communities.g" `
+    -C rlang `
+    2> $null 1> $null;
+}
+Write-Verbose "[Analysis Generation Completed] $($MeasureAnalysis.TotalSeconds)";
 
 $ClustersConfig | ForEach-Object {
   Remove-Item -Path "$($OutputDirectory)\intermediate.$($_.Code).g";
