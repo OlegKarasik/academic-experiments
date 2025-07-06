5c83addc518095b8e076f969c0e2f374da71aa75
Oleg Karasik <karasik.oleg.nikolaevich@gmail.com>
2025-07-06 15:05:08 +0300

    Started to lift common features to base "impl" class

--- src/_common/matrix-abstract.hpp
+++ src/_common/matrix-abstract.hpp
@@ -12,7 +12,14 @@ namespace matrices {
 //
 
 template<typename S, class Enable = void>
-struct matrix_abstract;
+class matrix_abstract;
+
+namespace impl {
+
+template<typename S>
+class impl_matrix_abstract;
+
+};
 
 //
 // Forward declarations
@@ -26,6 +33,7 @@ class matrix_abstract
 
 template<typename T, typename A>
 class matrix_abstract<square_matrix<T, A>, typename std::enable_if<traits::matrix_traits<T>::is_type::value>::type>
+  : public impl::impl_matrix_abstract<square_matrix<T, A>>
 {
 public:
   using matrix_type     = square_matrix<T, A>;
@@ -40,6 +48,13 @@ private:
 
 private:
   matrix_reference m_matrix;
+
+protected:
+  reference
+  translate_at(size_type i, size_type j) override
+  {
+    return this->m_matrix.at(i, j);
+  }
 
 public:
   matrix_abstract(matrix_reference matrix)
@@ -63,21 +78,11 @@ public:
   {
     return dimensions_type(this->m_matrix.size());
   }
-
-  reference
-  at(size_type i, size_type j)
-  {
-    return this->m_matrix.at(i, j);
-  }
-  reference
-  at(size_type i, size_type j) const
-  {
-    return this->m_matrix.at(i, j);
-  }
 };
 
 template<typename T, typename A, typename U>
 class matrix_abstract<square_matrix<square_matrix<T, A>, U>, typename std::enable_if<traits::matrix_traits<T>::is_type::value>::type>
+  : public impl::impl_matrix_abstract<square_matrix<square_matrix<T, A>, U>>
 {
 public:
   using matrix_type     = square_matrix<square_matrix<T, A>, U>;
@@ -97,22 +102,14 @@ private:
 
   std::vector<int> m_cache;
 
-private:
-  std::tuple<size_type, size_type>
-  translate_index(size_type index)
-  {
-    auto entry = this->m_cache[index];
-
-    return std::make_tuple(entry & 0xFFFF, entry >> 16);
-  }
-
+protected:
   reference
-  translate_at(size_type i, size_type j)
+  translate_at(size_type i, size_type j) override
   {
     size_type r, ro, c, co;
 
-    std::tie(r, ro) = this->translate_index(i);
-    std::tie(c, co) = this->translate_index(j);
+    std::tie(r, ro) = this->translation_entry_unpack(this->m_cache[i]);
+    std::tie(c, co) = this->translation_entry_unpack(this->m_cache[j]);
 
     return this->m_matrix.at(r, c).at(i - ro, j - co);
   }
@@ -140,7 +137,7 @@ public:
       auto size = this->m_matrix.at(z, z).size();
 
       for (auto i = size_type(0); i < size; ++i)
-        this->m_cache.push_back(int(delta << 16 | z));
+        this->m_cache.push_back(this->translation_entry_pack(z, delta));
 
       delta += size;
     }
@@ -157,21 +154,11 @@ public:
   {
     return this->m_matrix_dimensions;
   }
-
-  reference
-  at(size_type i, size_type j)
-  {
-    return this->translate_at(i, j);
-  }
-  reference
-  at(size_type i, size_type j) const
-  {
-    return this->translate_at(i, j);
-  }
 };
 
 template<typename T, typename A, typename U>
 class matrix_abstract<square_matrix<rect_matrix<T, A>, U>, typename std::enable_if<traits::matrix_traits<T>::is_type::value>::type>
+  : public impl::impl_matrix_abstract<square_matrix<rect_matrix<T, A>, U>>
 {
 public:
   using matrix_type     = square_matrix<rect_matrix<T, A>, U>;
@@ -192,22 +179,14 @@ private:
   std::vector<int> m_row_cache;
   std::vector<int> m_col_cache;
 
-private:
-  std::tuple<size_type, size_type>
-  translate_index(std::vector<int>& cache, size_type index)
-  {
-    auto entry = cache[index];
-
-    return std::make_tuple(entry & 0xFFFF, entry >> 16);
-  }
-
+protected:
   reference
-  translate_at(size_type i, size_type j)
+  translate_at(size_type i, size_type j) override
   {
     size_type r, ro, c, co;
 
-    std::tie(r, ro) = this->translate_index(this->m_row_cache, i);
-    std::tie(c, co) = this->translate_index(this->m_col_cache, j);
+    std::tie(r, ro) = this->translation_entry_unpack(this->m_row_cache[i]);
+    std::tie(c, co) = this->translation_entry_unpack(this->m_col_cache[j]);
 
     return this->m_matrix.at(r, c).at(i - ro, j - co);
   }
@@ -244,10 +223,10 @@ public:
       auto& block = this->m_matrix.at(z, z);
 
       for (auto i = size_type(0); i < block.height(); ++i, ++row)
-        this->m_row_cache.push_back(int(row_delta << 16 | z));
+        this->m_row_cache.push_back(this->translation_entry_pack(z, row_delta));
 
       for (auto j = size_type(0); j < block.width(); ++j,  ++col)
-        this->m_col_cache.push_back(int(col_delta << 16 | z));
+        this->m_col_cache.push_back(this->translation_entry_pack(z, col_delta));
 
       row_delta += block.height();
       col_delta += block.width();
@@ -265,18 +244,51 @@ public:
   {
     return this->m_matrix_dimensions;
   }
+};
 
+namespace impl {
+
+template<typename S>
+class impl_matrix_abstract
+{
+private:
+  using size_type = typename traits::matrix_traits<S>::size_type;
+
+  using reference       = typename traits::matrix_traits<S>::reference;
+  using const_reference = typename traits::matrix_traits<S>::const_reference;
+
+protected:
+  std::tuple<size_type, size_type>
+  translation_entry_unpack(int entry)
+  {
+    return std::make_tuple(entry & 0xFFFF, entry >> 16);
+  }
+
+  int
+  translation_entry_pack(size_type index, size_type offset)
+  {
+    return int(offset << 16 | index);
+  }
+
+protected:
+  virtual
+  reference
+  translate_at(size_type i, size_type j) = 0;
+
+public:
   reference
   at(size_type i, size_type j)
   {
     return this->translate_at(i, j);
   }
-  reference
+  const_reference
   at(size_type i, size_type j) const
   {
     return this->translate_at(i, j);
   }
 };
+
+}
 
 } // namespace matrices
 } // namespace utilz
